4d040b96fd4036e3199057a7a4a2035659c55aac
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-15 15:10:06 -0300

    Add previous project to the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import MovieList from './components/MovieList';
+import movies from './data';
+
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
