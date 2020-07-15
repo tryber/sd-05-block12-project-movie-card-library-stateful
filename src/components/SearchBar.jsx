45255750e1864eacc7a1e0624e9fae525b81c98a
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-15 18:27:27 -0300

    codeclimate 6

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,11 +5,11 @@ class SearchBar extends React.Component {
   // constructor(props) {
   //   super(props);
   // }
-  
+
   render() {
-    const { 
-      searchText, onSearchTextChange, bookmarkedOnly, 
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange 
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange
     } = this.props;
     return (
       <form>
