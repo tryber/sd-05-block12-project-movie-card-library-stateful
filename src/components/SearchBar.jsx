af4c52be64e9535f4a1c6b08ccf6a6e8efb7c5c6
pedrohcalado <pedrocalado22@gmail.com>
2020-07-15 16:43:12 -0300

    code climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,16 +9,25 @@ class SearchBar extends React.Component {
           <label htmlFor="text">
             Inclui o texto:
             <input
-            type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+              type="text"
+              value={this.props.searchText}
+              onChange={this.props.onSearchTextChange}
+            />
           </label>
           <label htmlFor="checkbox">
             Mostrar somente favoritos
             <input
-            type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+              type="checkbox"
+              checked={this.props.bookmarkedOnly}
+              onChange={this.props.onBookmarkedChange}
+            />
           </label>
           <label htmlFor="genre">
             Filtrar por gênero
-            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+            <select
+              value={this.props.selectedGenre}
+              onChange={this.props.onSelectedGenreChange}
+            >
               <option value="">Todos</option>
               <option value="action">Ação</option>
               <option value="comedy">Comédia</option>
