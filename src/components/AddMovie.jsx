979542e968d6db2ca951090fe64abd82510fa17f
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 20:19:23 -0300

    criando componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,16 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form>Teste</form>
+    );
+  }
+}
+
+export default AddMovie;
