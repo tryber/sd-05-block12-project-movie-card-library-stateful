11e5faf4854ad8a3029762ca9e02d199d72aebaf
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-15 17:49:47 -0300

    codeclimate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,21 +1,8 @@
 // implement AddMovie component here
 import React from 'react';
+import CreateInput from './CreateInput';
 
-class CreateInput extends React.Component {
-  render() {
-    return (
-      <label htmlFor={this.props.name}>
-        {this.props.title}
-        <input
-          type="text"
-          name={this.props.name}
-          value={this.props.value}
-          onChange={this.props.function}
-        />
-      </label>
-    )
-  }
-}
+
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
@@ -53,16 +40,12 @@ class AddMovie extends React.Component {
     return (
       <div>
         <form action="">
-          <CreateInput name="title" title="Título" value={this.state.title} function={this.handleChange}/>
-          <label htmlFor="subtitle">
-            Subtítulo
-            <input
-              type="text"
-              name="subtitle"
-              value={this.state.subtitle}
-              onChange={this.handleChange}
-            />
-          </label>
+          <CreateInput name="title" title="Título"
+            value={ this.state.title } function={ this.handleChange }
+          />
+          <CreateInput name="subtitle" title="Subtítulo"
+            value={this.state.subtitle} function={this.handleChange}
+          />
           <label htmlFor="image">
             Imagem
             <input
