e5cde3998c9d552742cfae40e8cea65a1cd6173b
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-15 10:53:24 -0300

    estrutura do componente movieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
 // implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    render() {
+      return(
+
+      )
+    }
+  }
+}
+
+export default MovieLibrary;
