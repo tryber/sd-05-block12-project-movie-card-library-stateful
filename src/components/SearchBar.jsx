787d7093ce2dd534e030a74634c432eac4e655d6
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-15 18:24:36 -0300

    SearchBar component implemented - codeclimate issues fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,30 +3,28 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     const {
-      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange } = this.props;
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+
     return (
-      <section>
-        <form>
-          <label htmlFor="searchText"> Inclui o texto:
-            <input id="searchText" type="text"
-              value={searchText} onChange={onSearchTextChange} />
-          </label>
-          <label htmlFor="checkbox"> Mostrar somente favoritos
-            <input id="checkbox" type="checkbox"
-              checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-          </label>
-          <label htmlFor="genero">Filtrar por gênero
-            <select id="genero" value={selectedGenre}
-              onChange={onSelectedGenreChange}>
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
-            </select>
-          </label>
-        </form>
-      </section>
+      <form>
+        <label htmlFor="searchText"> Inclui o texto:
+           <input id="searchText" type="text" value={searchText} onChange={onSearchTextChange} />
+        </label>
+        <label htmlFor="checkbox"> Mostrar somente favoritos
+          <input id="checkbox" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </label>
+        <label htmlFor="genero">Filtrar por gênero
+        <select id="genero" value={selectedGenre}
+            onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
