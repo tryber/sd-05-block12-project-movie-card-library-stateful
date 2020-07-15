1749a0caf68913865d47472c04c02de482b05a83
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-07-15 17:47:11 -0300

    iniciando o Projeto #VQV

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>Inclui o texto:</label>
+        <input type="text" value={searchText} />
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
