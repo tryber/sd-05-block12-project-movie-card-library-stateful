eccd8957ce8309f84508dff7e3607362c96c30a1
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-15 15:38:00 -0300

    Filmes e forms | Nat

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from '.components/MovieList';
+import SearchBar from '.components/SearchBar';
+import AddMovie from '.components/AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
