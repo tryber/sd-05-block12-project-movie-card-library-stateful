40ffb0ec3af2cd5f0b6e150aca7ea5d81f59d1f0
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-15 15:09:39 -0300

    climate issues

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,19 +4,22 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="searchbar">
-      <form>
-        <label>
-          Inclui o texto: <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
-        </label>
-        <label>Mostrar somente favoritos </label> <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}/>
-        <label>Filtrar por gênero</label>
-        <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-          <option value="">Todos</option>
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
-      </form>
+        <form>
+          <label>
+            Inclui o texto:
+            <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+          </label>
+          <label>Mostrar somente favoritos
+          </label>
+          <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}/>
+          <label>Filtrar por gênero</label>
+          <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+        </form>
       </div>
 
     );
