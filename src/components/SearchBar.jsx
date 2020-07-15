55bccc9214ebc67d7016215f38f5c1d13e2b5503
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-15 10:50:29 -0300

    Estruturaçao serach bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    render() {
+      return(
+
+      )
+    }
+  }
+}
+
+export default SearchBar;
