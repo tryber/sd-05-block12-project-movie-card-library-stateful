419664ebac1c93d56212283142b3d88c8e88e4fa
pedrohcalado <pedrocalado22@gmail.com>
2020-07-15 16:11:32 -0300

    code climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,20 +3,26 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return(
+    return (
       <div>
         <form>
-          <label>Inclui o texto:</label>
-          <input value={this.props.searchText} onChange={this.props.onSearchTextChange} />
-          <label>Mostrar somente favoritos</label>
-          <input type="checkbox" checked={this.props.bookmarkedOnly} />
-          <label>Filtrar por gênero</label>
-          <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
+          <label htmlFor="">
+            Inclui o texto:
+            <input value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+          </label>
+          <label>
+            Mostrar somente favoritos
+            <input type="checkbox" checked={this.props.bookmarkedOnly} />
+          </label>
+          <label>
+            Filtrar por gênero
+            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+              <option value="">Todos</option>
+              <option value="action">Ação</option>
+              <option value="comedy">Comédia</option>
+              <option value="thriller">Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
