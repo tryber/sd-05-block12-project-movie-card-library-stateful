b0e3c615559bd345c0bdc425536ef50a459c9b85
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 17:15:56 -0300

    incluindo form dentro de SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,29 +3,9 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
 
     return (
-      <form>
-        <label htmlFor="search-value">
-        Inclui o texto
-          <input
-            type="text"
-            value={searchText}
-            onChange={onSearchTextChange}
-            key="search-value"
-          />
-        </label>
-        <label htmlFor="filter-bookmarked">
-        Mostrar somente favoritos
-          <input
-            onChange={onBookmarkedChange}
-            checked={bookmarkedOnly}
-            type="checkbox"
-            key="filter-bookmarked"
-          />
-        </label>
-      </form>
+      <form></form>
     );
   }
 }
