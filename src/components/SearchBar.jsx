c7109e501c9642dda3934308e1377025690f3fbd
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-15 16:18:21 -0300

    Creating SearcBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,25 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+        Inclui o texto
+          <input
+            type="text"
+            value={this.props.searchText}
+            onChange={this.props.onSearchTextChange}
+          />
+        </label>
+
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
