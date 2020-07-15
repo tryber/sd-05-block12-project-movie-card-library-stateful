9277adf2fa774da82b17c122248d4c5110fdc106
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 16:51:30 -0300

    primeiro commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,39 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+    } = this.props;
+
+    return (
+      <form>
+        <label htmlFor="search-value">
+        Inclui o texto
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            key="search-value"
+          />
+        </label>
+        <label htmlFor="filter-bookmarked">
+        Mostrar somente favoritos
+          <input
+            onChange={onBookmarkedChange}
+            checked={bookmarkedOnly}
+            type="checkbox"
+            key="filter-bookmarked"
+          />
+        </label>
+
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
