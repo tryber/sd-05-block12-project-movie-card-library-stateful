42c245609cf9e505ef67c14196bc9cc58b653c0b
ambrosio <matmagamb1@gmail.com>
2020-07-15 18:49:48 -0300

    push 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,28 +3,31 @@
 import React from 'react';
 
 class Searchbar extends React.Component {
-
   render() {
-    //      onSelectedGenreChange,
-    //      selectedGenre,
-    //      onBookmarkedChange,
-    //      bookmarkedOnly
-
-    const { searchText, onSearchTextChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      onBookmarkedChange,
+      bookmarkedOnly,
+     } = this.props;
 
     return (
       <form type="submit">
         <label htmlFor="#filme">Inclui o texto:
           <div>
             <input
-              id="#filme"
-              type="text"
-              name="filme"
-              value={searchText}
+              id="#filme" type="text" name="filme" value={searchText}
               onChange={onSearchTextChange}
             />
           </div>
         </label>
+        <div>
+          <input
+            id="#favoritos"type="checkbox"
+            checked={bookmarkedOnly} onChange={onBookmarkedChange}
+          />
+          <label htmlFor="#favoritos">Mostrar somente favoritos</label>
+        </div>
       </form>
     );
   }
