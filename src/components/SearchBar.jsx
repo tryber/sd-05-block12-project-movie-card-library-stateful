886567231426c0bdf99a2ace9b1f4203a7471de8
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 17:29:51 -0300

    add input de texto dentro do form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form>Teste</form>
+      <form>
+        <label>
+          Inclui o Texto:
+          <input type="text" value={searchText} onChange={onSearchTextChange}/>
+        </label>
+      </form>
     );
   }
 }
