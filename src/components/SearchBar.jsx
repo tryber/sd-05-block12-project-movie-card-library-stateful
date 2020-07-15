cba8f0c40ae11b245ee8daee2de810d522e0cd05
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-15 19:09:48 -0300

    SearchBar component implemented - codeclimate issues fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,19 @@
   import React, { Component } from 'react';
 
   class SearchBar extends Component {
-  render() {
-  const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
-  const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+    const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
-  return (
-  <form>
+    return (
+   <form>
     <label htmlFor="searchText"> Inclui o texto:
       <input type="text" value={searchText} onChange={onSearchTextChange} />
     </label>
-  <label htmlFor="checkbox"> Mostrar somente favoritos
+    <label htmlFor="checkbox"> Mostrar somente favoritos
     <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-  </label>
-  <label htmlFor="genero">Filtrar por gênero
+    </label>
+    <label htmlFor="genero">Filtrar por gênero
     <select id="genero" value={selectedGenre} onChange={onSelectedGenreChange}>
       <option value="">Todos</option>
       <option value="action">Ação</option>
