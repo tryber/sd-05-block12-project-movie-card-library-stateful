d6d60485f1eaac1b026ae956d877c93e2a7c52e1
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 20:36:44 -0300

    add componente MovieLibrary.jsx

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,20 @@
 // implement MovieLibrary component here
+import React from 'react';
+import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies
+    }
+  }
+  render() {
+    return (
+      <div>teste</div>
+    );
+  }
+}
