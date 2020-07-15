4f0ca6fb4e262fd65fb9fe9c708b6cb47f098891
pedrohcalado <pedrocalado22@gmail.com>
2020-07-15 16:49:02 -0300

    code climate :))))))

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,10 +7,14 @@ class SearchBar extends React.Component {
       <div>
         <form>
           <label htmlFor="text">Inclui o texto:
-            <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+            <input type="text" value={this.props.searchText}
+              onChange={this.props.onSearchTextChange}
+            />
           </label>
           <label htmlFor="checkbox">Mostrar somente favoritos
-            <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+            <input type="checkbox" checked={this.props.bookmarkedOnly}
+              onChange={this.props.onBookmarkedChange}
+            />
           </label>
           <label htmlFor="genre">Filtrar por gênero
             <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
