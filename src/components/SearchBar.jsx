75cd6438944e640b8b1bf860913a1c498bf19c06
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 17:35:09 -0300

    add input checkbox dentro do form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <form>
         <label>
           Inclui o Texto:
           <input type="text" value={searchText} onChange={onSearchTextChange}/>
+        </label>
+        <label>
+          Mostrar somente favoritos
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
         </label>
       </form>
     );
