b36d846b429547017ec01630013acce76c443df1
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-15 17:54:42 -0300

    requirement 1 solved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import movies from './data';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
