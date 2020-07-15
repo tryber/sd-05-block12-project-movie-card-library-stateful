6446967c4e73ac3110e08f9d7c7ac7ea04ed3938
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-15 10:52:39 -0300

    implementaçao da estrutura de addMovie e MovieLibrary

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,19 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    render() {
+      return(
+
+      )
+    }
+  }
+}
+
+export default AddMovie;
