736b776d1659a1da4b0f85880043e601250ba6e8
luialbeto <araujo.script@gmail.com>
2020-07-15 16:10:59 -0300

    intial commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+const searchBar = () => (
+  mount(
+    <SearchBar
+      searchText={props.searchText}
+      onSearchTextChange={props.onSearchTextChange}
+      bookmarkedOnly={props.bookmarkedOnly}
+      onBookmarkedChange={props.onBookmarkedChange}
+      selectedGenre={props.selectedGenre}
+      onSelectedGenreChange={props.onSelectedGenreChange}
+    />,
+  )
+  )}
+  );
+}
+
+export default SearchBar;
