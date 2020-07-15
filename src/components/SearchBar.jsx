d2b534c4eb50a785ac9dfa0842a30cf9bebd7ec8
marinabrcls <barcelos.marina@gmail.com>
2020-07-15 17:50:06 -0300

    search bar implementada e passando nos testes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,53 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <section>
+        <form>
+          <label>
+            Inclui o texto:
+            <input
+              type='text'
+              value={searchText}
+              onChange={onSearchTextChange}
+            />
+          </label>
+          <label>
+            Mostrar somente favoritos
+            <input
+              type='checkbox'
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
+            />
+          </label>
+          <label>
+            Filtrar por gênero
+            <select
+              name='genero'
+              value={selectedGenre}
+              onChange={onSelectedGenreChange}
+            >
+              <option value=''>Todos</option>
+              <option value='action'>Ação</option>
+              <option value='comedy'>Comédia</option>
+              <option value='thriller'>Suspense</option>
+            </select>
+          </label>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default SearchBar;
