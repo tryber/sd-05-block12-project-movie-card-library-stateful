9583708c2bfa9931303eb616bf17837eda9095bf
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-15 19:05:57 -0300

    SearchBar component implemented - codeclimate issues fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,23 +7,23 @@
 
   return (
   <form>
-  <label htmlFor="searchText"> Inclui o texto:
-  <input type="text" value={searchText} onChange={onSearchTextChange} />
-  </label>
+    <label htmlFor="searchText"> Inclui o texto:
+      <input type="text" value={searchText} onChange={onSearchTextChange} />
+    </label>
   <label htmlFor="checkbox"> Mostrar somente favoritos
-  <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+    <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
   </label>
   <label htmlFor="genero">Filtrar por gênero
-  <select id="genero" value={selectedGenre} onChange={onSelectedGenreChange}>
-  <option value="">Todos</option>
-  <option value="action">Ação</option>
-  <option value="comedy">Comédia</option>
-  <option value="thriller">Suspense</option>
-</select>
+    <select id="genero" value={selectedGenre} onChange={onSelectedGenreChange}>
+      <option value="">Todos</option>
+      <option value="action">Ação</option>
+      <option value="comedy">Comédia</option>
+      <option value="thriller">Suspense</option>
+    </select>
   </label>
   </form>
   );
   }
-  }
+}
 
   export default SearchBar;
