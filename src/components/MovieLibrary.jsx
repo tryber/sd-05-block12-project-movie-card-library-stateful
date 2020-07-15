3a3459a50c5c3f430d4d4796cb826b57fbdb86a1
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-15 11:57:54 -0300

    implementaçao de movielibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,42 @@
 // implement MovieLibrary component here
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
+    this.state = { 
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
     };
+  }
+  
+  onSearchTextChange = () => {
+
+  }
+
+  onBookmarkedChange = () => {
+
+  }
+
+  onSelectedGenreChange = () => {
+
   }
 
   render() {
     return(
+      <div>
+        <SearchBar
+          searchText={this.state.searchText} 
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
+      </div>
 
     )
   }
