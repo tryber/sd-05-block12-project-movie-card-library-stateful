ecf188119d5401c63714bf97f3bead58302bb59e
pedrohcalado <pedrocalado22@gmail.com>
2020-07-15 17:09:20 -0300

    code climateeeeeeeee

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,63 @@
 // implement SearchBar component here
 import React from 'react';
 
+
+
 class SearchBar extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  
+  TextInput = () => {
+    return (
+      <label htmlFor="text">
+      Inclui o texto:
+      <input
+        type="text"
+        value={this.props.searchText}
+        onChange={this.props.onSearchTextChange}
+      />
+    </label>
+    );
+  }
+
+  CheckBox = () => {
+    return (
+      <label htmlFor="checkbox">
+      Mostrar somente favoritos
+      <input
+        type="checkbox"
+        checked={this.props.bookmarkedOnly}
+        onChange={this.props.onBookmarkedChange}
+      />
+    </label>
+    );
+  }
+
+  Genre = () => {
+    return (
+      <label htmlFor="genre">
+        Filtrar por gênero
+        <select 
+          value={this.props.selectedGenre}
+          onChange={this.props.onSelectedGenreChange}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <div>
         <form>
-          <label htmlFor="text">Inclui o texto:
-            <input type="text"
-              value={this.props.searchText}
-              onChange={this.props.onSearchTextChange}
-            />
-          </label>
-          <label htmlFor="checkbox">Mostrar somente favoritos
-            <input type="checkbox"
-              checked={this.props.bookmarkedOnly}
-              onChange={this.props.onBookmarkedChange}
-            />
-          </label>
-          <label htmlFor="genre">Filtrar por gênero
-            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
-            </select>
-          </label>
+          {this.TextInput()}
+          {this.CheckBox()}
+          {this.Genre()}
         </form>
       </div>
     );
