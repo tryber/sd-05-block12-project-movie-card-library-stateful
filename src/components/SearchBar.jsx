9f2193071ffadfa47d26320b2b6957e2ff6c1905
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-15 17:39:33 -0300

    corrigindo code climate - incluindo htmlFor nas labels

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,13 +6,13 @@ class SearchBar extends React.Component {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <form>
-        <label>
+        <label htmlFor="search-value">
           Inclui o Texto:
-          <input type="text" value={searchText} onChange={onSearchTextChange}/>
+          <input type="text" value={searchText} onChange={onSearchTextChange} />
         </label>
-        <label>
+        <label htmlFor="filter-bookmarked">
           Mostrar somente favoritos
-          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </label>
       </form>
     );
