873394f735c58bd7dff41faac1bbb0d41ecf245d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-15 16:09:43 -0300

    props of SearchBar set

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        {this.props.searchText}
+        {this.props.onSearchTextChange}
+        {this.props.bookmarkedOnly}
+        {this.props.onBookmarkedChange}
+        {this.props.selectedGenre}
+        {this.props.onSelectedGenreChange}
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
