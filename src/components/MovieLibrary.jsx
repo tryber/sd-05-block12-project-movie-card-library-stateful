a298ad7f39fbe1c3d89b5fab95824a9d3e76eaf2
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-07-15 13:10:34 -0300

    pesquisa funcionando no componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,8 @@
 // implement MovieLibrary component here
 import React from 'react';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -13,15 +15,56 @@ class MovieLibrary extends React.Component {
     };
   }
   
-  onSearchTextChange = () => {
-
+  onSearchTextChange = async (event) => {
+    const { value } = event.target;
+    await this.setState({ searchText: value });
+    const movieList = this.props.movies;
+    const searchTerm = this.state.searchText;
+    const filteredList = movieList.filter((movie) => {
+      if (movie.title.includes(searchTerm)) {
+        return movie;
+      }
+      if (movie.subtitle.includes(searchTerm)) {
+        return movie;
+      }
+      if (movie.storyline.includes(searchTerm)) {
+        return movie;
+      }
+    });
+    this.setState({ movies: filteredList });
   }
 
-  onBookmarkedChange = () => {
-
+  onBookmarkedChange = async (event) => {
+    const { checked } = event.target;
+    await this.setState({ bookmarkedOnly: checked });
+    const movieList = this.props.movies;
+    const marked = this.state.bookmarkedOnly;
+    if (marked === true) {
+      const filteredList = movieList.filter((movie) => {
+        if (movie.bookmarked === true) {
+          return movie;
+        }
+      });
+      this.setState({ movies: filteredList });
+    } else {
+      this.setState({ movies: this.props.movies });
+    }
   }
 
-  onSelectedGenreChange = () => {
+  onSelectedGenreChange = async (event) => {
+    const { value } = event.target;
+    await this.setState({selectedGenre: value});
+    const movieList = this.props.movies;
+    const selectedGenre = this.state.selectedGenre;
+    const filteredList = movieList.filter((movie) => {
+      if (movie.genre === selectedGenre || selectedGenre === '') {
+        return movie
+      }
+    });
+    this.setState({ movies: filteredList });
+  }
+
+  onClick = () => {
 
   }
 
@@ -36,6 +79,8 @@ class MovieLibrary extends React.Component {
           selectedGenre={this.state.selectedGenre}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
+        <MovieList movies={this.state.movies} />
+        <AddMovie onClick={this.onClick} />
       </div>
 
     )
