6c1169392833b890f68e9c44037db5191f84e063
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-15 16:59:26 -0300

    fixing CC using destruturing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,23 +7,32 @@ class SearchBar extends Component {
   // }
   
   render() {
+    const {
+      onSearchTextChange,
+      searchText,
+      onSelectedGenreChange,
+      selectedGenre,
+      onBookmarkedChange,
+      bookmarkedOnly,
+    } = this.props;
+
     return (
       <form>
         <label htmlFor="text">
           Inclui o texto:
-          <input type='text' value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+          <input type="text" value={searchText} onChange={onSearchTextChange} />
         </label>
         <label htmlFor="favorites">
           Mostrar somente favoritos
-          <input type='checkbox' checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </label>
         <label htmlFor="style">
           Filtrar por gênero
-          <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-            <option value=''>Todos</option>
-            <option value='action'>Ação</option>
-            <option value='comedy'>Comédia</option>
-            <option value='thriller'>Suspense</option>
+          <select value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
           </select>
         </label>
       </form>
