b8616a0c687c57644ba1058ef9ff8351f12a6a82
Felipe Vieira <flpvieira86@gmail.com>
2020-07-15 19:00:20 -0300

    MovieLib adicionada no App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
 
+import movies from './data';
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
+      <MovieLibrary movies={movies} />
     </div>
   );
 }
