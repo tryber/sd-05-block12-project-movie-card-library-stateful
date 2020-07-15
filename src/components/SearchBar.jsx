d1e2b2979d7cf2ee09a6577f9fb8274e726d306c
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-15 16:53:28 -0300

    fixing more CC on form: input htmlFor and render size

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,36 +5,28 @@ class SearchBar extends Component {
   // constructor(props) {
   //   super(props);
   // }
-
+  
   render() {
     return (
-        <form>
-          <label>Inclui o texto:
-          <input
-          type="text"
-            value={this.props.searchText}
-            onChange={this.props.onSearchTextChange}
-          />
-          </label>
-          <label>Mostrar somente favoritos
-          <input
-            type="checkbox"
-            checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}
-          />
-          </label>
-          <label>Filtrar por gênero
-          <select
-            value={this.props.selectedGenre}
-            onChange={this.props.onSelectedGenreChange} 
-          >
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+      <form>
+        <label htmlFor="text">
+          Inclui o texto:
+          <input type='text' value={this.props.searchText} onChange={this.props.onSearchTextChange} />
+        </label>
+        <label htmlFor="favorites">
+          Mostrar somente favoritos
+          <input type='checkbox' checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} />
+        </label>
+        <label htmlFor="style">
+          Filtrar por gênero
+          <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+            <option value=''>Todos</option>
+            <option value='action'>Ação</option>
+            <option value='comedy'>Comédia</option>
+            <option value='thriller'>Suspense</option>
           </select>
-          </label>
-        </form>
+        </label>
+      </form>
     );
   }
 }
