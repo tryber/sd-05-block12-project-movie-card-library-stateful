1eb8828fab1038f49575ced8ab480e9a7839ed3f
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-15 18:56:19 -0300

    SearchBar component implemented - codeclimate issues fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 
-class SearchBar extends Component {
-render() {
-const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
-const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+  class SearchBar extends Component {
+  render() {
+  const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+  const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
-return (
-<form>
-<label htmlFor="searchText"> Inclui o texto:
+  return (
+  <form>
+  <label htmlFor="searchText"> Inclui o texto:
   <input type="text" value={searchText} onChange={onSearchTextChange} />
-</label>
-<label htmlFor="checkbox"> Mostrar somente favoritos
-<input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-</label>
-<label htmlFor="genero">Filtrar por gênero
-<select id="genero" value={selectedGenre} onChange={onSelectedGenreChange}>
+  </label>
+  <label htmlFor="checkbox"> Mostrar somente favoritos
+  <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+  </label>
+  <label htmlFor="genero">Filtrar por gênero
+  <select id="genero" value={selectedGenre} onChange={onSelectedGenreChange}>
   <option value="">Todos</option>
   <option value="action">Ação</option>
   <option value="comedy">Comédia</option>
   <option value="thriller">Suspense</option>
-  </select>
-</label>
-</form>
-);
-}
-}
+</select>
+  </label>
+  </form>
+  );
+  }
+  }
 
-export default SearchBar;
+  export default SearchBar;
