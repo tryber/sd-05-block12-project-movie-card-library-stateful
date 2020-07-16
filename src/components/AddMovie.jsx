78b3ad2c6e2c9ee7fbd48f6c598c5631d7d70044
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-16 15:39:41 -0300

    Criação do component InputElement

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import InputElement from './InputElement';
 
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      subtitle: "",
-      title: "",
-      imagePath: "",
-      storyline: "",
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
       rating: 0,
-      genre: "action",
+      genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleChangeNum = this.handleChangeNum.bind(this);
@@ -30,51 +31,23 @@ class AddMovie extends React.Component {
     const onClick = this.props.onClick;
     onClick(this.state);
     this.setState({
-      subtitle: "",
-      title: "",
-      imagePath: "",
-      storyline: "",
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
       rating: 0,
-      genre: "action",
+      genre: 'action',
     });
   }
 
   render() {
     const { subtitle, title, imagePath, storyline, genre } = this.props;
     return (
-      <div>
-        <form>
-          <label>
-            Título
-            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
-          </label>
-          <label>
-            Subtítulo
-            <input type="text" name="subtitle" value={this.state.subtitle} onChange={this.handleChange} />
-          </label>
-          <label>
-            Imagem
-            <input type="text" name="imagePath" value={this.state.imagePath} onChange={this.handleChange} />
-          </label>
-          <label>
-            Sinopse
-            <textarea name="storyline" value={this.state.storyline} onChange={this.handleChange} />
-          </label>
-          <label>
-            Avaliação
-            <input type="number" name="rating" value={this.state.rating} onChange={this.handleChangeNum} />
-          </label>
-          <label>
-            Gênero
-            <select name="genre" value={this.state.genre} onChange={this.handleChange}>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
-            </select>
-          </label>
-          <button onClick={this.NewMovie}>Adicionar filme</button>
-        </form>
-      </div>
+      <form>
+        <InputElement 
+          label="title" titulo="Título" value={this.state.title} change={this.handleChange}
+        />
+      </form>
     );
   }
 }
