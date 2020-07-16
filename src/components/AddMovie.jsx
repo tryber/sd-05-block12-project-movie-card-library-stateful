c7e3894cbdcbbfff85d6e5ab08ce9b51d53e5a5d
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-15 22:39:13 -0300

    climate issue

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-class AddMovie extends React.Component{
-  constructor(props){
+class AddMovie extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       subtitle: '',
@@ -9,17 +9,15 @@ class AddMovie extends React.Component{
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
-    }
+      genre: 'action',
+    };
     this.resetaEstados = this.resetaEstados.bind(this);
     this.onChangeHandle = this.onChangeHandle.bind(this);
   }
 
   onChangeHandle(event) {
     const { name, value } = event.target;
-    this.setState({
-      [name]: (name === 'rating') ? parseFloat(value) : value
-    });
+    this.setState({ [name]: (name === 'rating') ? parseFloat(value) : value });
     console.log(this.state);
   }
 
@@ -30,24 +28,32 @@ class AddMovie extends React.Component{
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
+      genre: 'action',
     });
+  }
+  economizaLinha() {
+    return (
+      <input
+        id="c" type="text" name="imagePath" value={this.props.imagePath}
+        onChange={this.props.onChangeHandle}
+      />
+    );
   }
 
   render() {
-    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    const { title, subtitle, storyline, rating, genre } = this.state;
     return (
       <form className="AddMove">
-        <label htmlFor="teste">Título</label>
-        <input id="teste" name="title" type="text" value={title} onChange={this.onChangeHandle} />
-        <label htmlFor="teste1">Subtítulo</label>
-        <input id="teste1" type="text" name="subtitle" value={subtitle} onChange={this.onChangeHandle} />
-        <label htmlFor="teste2">Imagem</label>
-        <input id="teste2" type="text" name="imagePath" value={imagePath} onChange={this.onChangeHandle} />
-        <label htmlFor="teste3">Sinopse</label>
-        <textarea id="teste3" name="storyline" value={storyline} onChange={this.onChangeHandle} />
-        <label htmlFor="teste4">Avaliação</label>
-        <input type="number" id="teste4" name="rating" value={rating} onChange={this.onChangeHandle} />
+        <label htmlFor="a">Título</label>
+        <input id="a" name="title" type="text" value={title} onChange={this.onChangeHandle} />
+        <label htmlFor="b">Subtítulo</label>
+        <input id="b" type="text" name="subtitle" value={subtitle} onChange={this.onChangeHandle} />
+        <label htmlFor="c">Imagem</label>
+        {this.economizaLinha()}
+        <label htmlFor="d">Sinopse</label>
+        <textarea id="d" name="storyline" value={storyline} onChange={this.onChangeHandle} />
+        <label htmlFor="e">Avaliação</label>
+        <input type="number" id="e" name="rating" value={rating} onChange={this.onChangeHandle} />
         <label htmlFor="selectedGenre">Gênero</label>
         <select
           id="selectedGenre" name="genre" value={genre}
