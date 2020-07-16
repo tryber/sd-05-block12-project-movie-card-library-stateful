99dbcf74bb1a0932f595b046542336ba397276c2
marinabrcls <barcelos.marina@gmail.com>
2020-07-16 20:14:56 -0300

    code climate fixed

--- src/components/BoxOptions.jsx
+++ src/components/BoxOptions.jsx
@@ -5,7 +5,11 @@ class BoxOptions extends React.Component {
     return (
       <label htmlFor={this.props.label}>
         {this.props.titulo}
-        <select name={this.props.label} value={this.props.value} onChange={this.props.change}>
+        <select
+          name={this.props.label}
+          value={this.props.value}
+          onChange={this.props.change}
+        >
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
