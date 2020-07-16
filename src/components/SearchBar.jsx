a9fdd6583cf29e71981b71ae1a6e5fd2d775c7a8
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-15 21:10:30 -0300

    SearchBar added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+      <label>
+        Inclui o texto: <br></br>
+        <input className="searchText" type="text" />
+      </label>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
