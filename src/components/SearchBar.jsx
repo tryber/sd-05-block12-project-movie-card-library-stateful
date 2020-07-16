8f6861b2fa1634c47805371c7f74fbf8154c6d78
ferrari <luisdiasferrari@gmail.com>
2020-07-16 16:13:24 -0300

    Formulario done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,76 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
+  incluir() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form className="formulario">
-        <div className="search">
-          <label>
-          Inclui o texto:<br></br>
+      <div>
+        <label htmlFor="searchText" >
+          Inclui o texto:<br />
           <input
-            className="searchText caixaImput" type="text"
-            value={searchText} onChange={onSearchTextChange}
+            className="searchText caixaImput"
+            name="searchText"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
           />
-          </label>
-        </div>
-        <div className="search">
-          <label>
-          <input className="caixaCheck" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-          Mostrar somente favoritos
-          </label>
-        </div>
-        <div>
-          <label>
-            Filtrar por gênero<br></br>
-          <select className="caixaImput" value={selectedGenre} onChange={onSelectedGenreChange}>
+        </label>
+      </div>
+    );
+  }
+
+  favorito() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <div>
+        <label htmlFor="caixaCheck">
+          <input
+            className="caixaCheck"
+            name="caixaCheck"
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
+        Mostrar somente favoritos
+        </label>
+      </div>
+    );
+  }
+
+  filtroGenero() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        <label htmlFor="caixaImput">
+          Filtrar por gênero<br />
+          <select
+            className="caixaImput"
+            name="caixaImput"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
             <option value="thriller">Suspense</option>
           </select>
-          </label>
+        </label>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form className="formulario">
+        <div>
+          {this.incluir()}
+        </div>
+        <div>
+          {this.favorito()}
+        </div>
+        <div>
+          {this.filtroGenero()}
         </div>
       </form>
     );
