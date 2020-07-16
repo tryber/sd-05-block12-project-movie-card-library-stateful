7d6ef069b39e1a8e02a2fd9004e6805f7f31b525
ferrari <luisdiasferrari@gmail.com>
2020-07-16 14:30:34 -0300

    Formulario done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      <section>
+      <form className="formulario">
         <div className="search">
-          <p>Inclui o texto:</p><br />
+          <label>
+          Inclui o texto:<br></br>
           <input
-            className="searchText" type="text"
+            className="searchText caixaImput" type="text"
             value={searchText} onChange={onSearchTextChange}
           />
+          </label>
         </div>
         <div className="search">
-          <p>Mostrar somente favoritos</p>
-          <input type="checkbox" checked="bookmarkedOnly" onChange="onBookmarkedChange" />
+          <label>
+          <input className="caixaCheck" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          Mostrar somente favoritos
+          </label>
         </div>
-      </section>
+        <div>
+          <label>
+            Filtrar por gênero<br></br>
+          <select className="caixaImput" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+          </label>
+        </div>
+      </form>
     );
   }
 }
