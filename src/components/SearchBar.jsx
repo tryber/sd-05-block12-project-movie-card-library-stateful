907de1a976a2c3dac328911f29777b90db513c43
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-15 23:30:17 -0300

    checkbox added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: 'My Text' };
+  }
+
+  onSearchTextChange(event) {
+    this.setState({ search: event.target.value });
+  }
+
   render() {
     return (
       <form>
-          Inclui o texto:
-        <input id="searchText" type="text" />
+        <label htmlFor="searchText">Inclui o texto:
+          <input
+            onChange={this.onSearchTextChange.bind}
+            className="searchText"
+            type="text"
+            id="searchText"
+            value={this.state.search}
+            name="searchText"
+          />
+        </label>
+        <label htmlFor="Favorites">Mostrar somente favoritos
+        </label>
+        <input
+          type="checkbox"
+          id="Favorites" check="checked"
+        />
       </form>
     );
   }
