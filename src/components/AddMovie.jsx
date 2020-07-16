ccd94d5ac53d9b5e3d11d3538dadc23699e356dd
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-07-16 05:38:50 -0300

    salvando progresso, travei em AddMovie e preciso dormir urgente!

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,10 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    const { onClick } = this.props;
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,48 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form className="movie-card-search">
+        <label htmlFor="searchText">
+          Inclui o texto:
+          <input
+            type="text"
+            name="searchtext"
+            value="{searchText}"
+            onChange="{onSearchTextChange}"
+          />
+        </label>
+        <label htmlFor="bookMarker">
+          Mostrar somente favoritos
+          <input
+            type="checklist"
+            checked="{bookmarkedOnly}"
+            onChange="{onBookmarkedChange}"
+          />
+        </label>
+        <label htmlFor="selector">
+          Filtrar por gênero
+          <select
+            name="genreFilter"
+            id="genreFilter"
+            value="{selectedGenre}"
+            onChange="{onSelectedGenreChange}"
+          ></select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
