e5672e5b1ff68d34b4edcd341f9cb7d1eb0c4217
ambrosio <matmagamb1@gmail.com>
2020-07-16 16:53:29 -0300

    push 6

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import TextBar from './TextBar';
 import CheckBoxBar from './CheckBoxBar';
+import SelectBar from './SelectBar';
 
 
 class searchBar extends React.Component {
@@ -12,12 +13,15 @@ class searchBar extends React.Component {
       onSearchTextChange,
       onBookmarkedChange,
       bookmarkedOnly,
+      selectedGenre,
+      onSelectedGenreChange,
      } = this.props;
 
     return (
       <form type="submit">
         <TextBar value={searchText} change={onSearchTextChange} />
         <CheckBoxBar check={bookmarkedOnly} change={onBookmarkedChange} />
+        <SelectBar values={selectedGenre} changes={onSelectedGenreChange}/>
       </form>
     );
   }
