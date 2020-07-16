12b451e37f7810f662d2503a096d0e814af56b25
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-16 14:35:12 -0300

    configurando estado inicial do componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,40 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
   render() {
     return (
-      <form>Teste</form>
+      <form>
+        <label htmlFor="sub ">
+          <input type="text"/>
+        </label>
+        <label htmlFor="title">
+          <input type="text"/>
+        </label>
+        <label htmlFor="img">
+          <input type="text"/>
+        </label>
+        <label htmlFor="sinopse">
+          <input type="text"/>
+        </label>
+        <label htmlFor="avaliacao">
+          <input type="text"/>
+        </label>
+        <label htmlFor="genero">
+          <input type="text"/>
+        </label>
+      </form>
     );
   }
 }
