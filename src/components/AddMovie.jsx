f07f468ac5e9d06fd409b27dae87402e4404d16e
pedrohcalado <pedrocalado22@gmail.com>
2020-07-16 10:31:15 -0300

    AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,14 +12,21 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
-    this.insertTitle = this.insertTitle.bind(this);
+    
     this.handleChange = this.handleChange.bind(this);
+    this.insertTitle = this.insertTitle.bind(this);
     this.insertSubtitle = this.insertSubtitle.bind(this);
+    this.insertImage = this.insertImage.bind(this);
+    this.insertSinopse = this.insertSinopse.bind(this);
+    this.insertRating = this.insertRating.bind(this);
+    this.insertGenre = this.insertGenre.bind(this);
+    this.insertButton = this.insertButton.bind(this);
   }
 
   handleChange(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: name === 'rating' ? parseFloat(value) : value });
+    console.log(this.state)
   }
 
   insertTitle() {
@@ -38,7 +45,7 @@ class AddMovie extends React.Component {
 
   insertSubtitle() {
     return (
-      <label htmlFor="title">
+      <label htmlFor="subtitle">
         Subtítulo
         <input
           name="subtitle"
@@ -50,12 +57,86 @@ class AddMovie extends React.Component {
     );
   }
 
+  insertImage() {
+    return (
+      <label htmlFor="imagePath">
+        Imagem
+        <input
+          name="imagePath"
+          type="text"
+          value={this.state.imagePath}
+          onChange={this.handleChange}
+        />
+      </label>
+    );
+  }
+
+  insertSinopse() {
+    return (
+      <label htmlFor="storyline">
+        Sinopse
+        <textarea
+          name="storyline"
+          type="text"
+          value={this.state.storyline}
+          onChange={this.handleChange}
+        />
+      </label>
+    );
+  }
+
+  insertRating() {
+    return (
+      <label htmlFor="rating">
+        Avaliação
+        <input
+          name="rating"
+          type="number"
+          value={this.state.rating}
+          onChange={this.handleChange}
+        />
+      </label>
+    );
+  }
+
+  insertGenre() {
+    return (
+      <label>
+        Gênero
+        <select
+          name="genre"
+          value={this.state.genre}
+          onChange={this.handleChange}
+        >
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  insertButton() {
+    return (
+      <button
+        onClick={this.resetState}
+      >
+        Adicionar filme
+      </button>
+    );
+  }
+
   render() {
     return (
       <div>
         <form>
           {this.insertTitle()}
           {this.insertSubtitle()}
+          {this.insertImage()}
+          {this.insertSinopse()}
+          {this.insertRating()}
+          {this.insertGenre()}
+          {this.insertButton()}
         </form>
       </div>
     );
