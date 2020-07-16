e505feffdba28848423751a443d3c33d045e919e
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-16 18:40:38 -0300

    Outra tentativa

---
+++ src/components/BoxOptions.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class BoxOptions extends React.Component {
+  render() {
+    return (
+      <label htmlFor={this.props.label}>
+        {this.props.titulo}
+        <select name={this.props.label} value={this.props.value} onChange={this.props.change}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+}
+
+export default BoxOptions;
