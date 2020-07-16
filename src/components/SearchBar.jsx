9a5a63080b73952bbe842ce4d67d9902625181a9
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-15 23:05:52 -0300

    add quick fix to SearchBar.js

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,46 +1,60 @@
 import React, { Component } from 'react';
 
+const Options = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="genrefilter">
+      Gênero
+      <select
+        value={value}
+        onChange={onChange}
+        name="genre-filter" id="genre-filter"
+      >
+        <option value="">Todos</option>
+        <option value="action">Ação</option>
+        <option value="comedy">Comédia</option>
+        <option value="thriller">Suspense</option>
+      </select>
+    </label>
+  </div>
+);
+
+const SearchTextFunc = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="text">
+      Inclui o texto:
+      <input
+        type="text"
+        name="text"
+        id="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  </div>
+);
+// ANCHOR class
+
 class SearchBar extends Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
+      searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange,
     } = this.props;
 
     return (
       <fieldset>
         <form>
-          <label> 
-            Inclui o texto:
-            <input type="text" value={searchText} onChange={onSearchTextChange} />
-          </label>
+          <SearchTextFunc value={searchText} onChange={onSearchTextChange} />
+          <label htmlFor="checkbox">Mostrar somente favoritos</label>
+          <input
+            type="checkbox" name="checkbox"
+            id="checkbox" checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
 
-          <label htmlFor="checkbox">
-            Mostrar somente favoritos
-            <input
-              type="checkbox"
-              name="checkbox"
-              id="checkbox"
-              checked={bookmarkedOnly}
-              onChange={onBookmarkedChange} 
-            />
-          </label>
+          <label htmlFor="genre-filter">Filtrar por gênero</label>
 
-        <label>
-          Filtrar por gênero
-          <select name="genre-filter" id="genre-filter" value={selectedGenre} onChange={onSelectedGenreChange}>
-
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
-
+          <Options value={selectedGenre} onChange={onSelectedGenreChange} />
         </form>
       </fieldset>
     );
@@ -48,18 +62,3 @@ class SearchBar extends Component {
 }
 
 export default SearchBar;
-
-// Renderize um select dentro do formulário em SearchBar
-// O select deve ter uma label associada com o texto: "Filtrar por gênero";
-
-// A propriedade value do select deve receber o valor da prop selectedGenre;
-
-// A propriedade onChange do input deve receber o valor da prop onSelectedGenreChange;
-
-// O select deve renderizar quatro tags option, com as opções de filtragem por gênero, na seguinte ordem:
-
-// Todos, com o valor "";
-// Ação, com o valor action;
-// Comédia, com o valor comedy;
-// Suspense, com o valor thriller.
-// Crie um componente chamado AddMovie
