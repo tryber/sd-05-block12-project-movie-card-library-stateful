4eb71822355e9c73fdcfcdeb68ad97a7a1088ba3
Marylange <marylangess@gmail.com>
2020-07-16 11:22:15 -0300

    Iniciando projeto movie cards library stateful

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ class Header extends React.Component {
     return (
       <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
+        <h2>VQV!</h2>
       </header>
     );
   }
