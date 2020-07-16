08081705e5e53dbca2d38991bddfc34545b3bec6
pedrohcalado <pedrocalado22@gmail.com>
2020-07-16 10:43:58 -0300

    Create Movie lib

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,23 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
+
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
+
+export default MovieLibrary;
