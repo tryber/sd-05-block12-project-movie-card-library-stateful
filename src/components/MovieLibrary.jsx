8aa2297c91cc2d00db407788974a85bda3342b08
RenataNovais <rnovaiscs@gmail.com>
2020-07-16 11:56:22 -0300

    trying to do some requirements

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    }
+  }
+
+  handleChange(event) {
+    this.setState = {
+      searchText: event.target.value,
+      bookmarkedOnly: true,
+      selectedGenre: event.target.value,
+    }
+  }
+
+  render() {
+    <div>
+      <SearchBar
+        searchText={this.state.searchText}
+        onSearchTextChange={() => this.handleChange}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        onBookmarkedChange={() => this.handleChange}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenreChange={() => this.handleChange} />
+    </div>
+  }
+}
+
+export default MovieLibrary;
