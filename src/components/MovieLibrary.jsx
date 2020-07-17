5378cfa6c33b76b23d3fb0c422b73539eaf0f813
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-17 14:58:41 -0300

    Creating MovieLibrary and setting states

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
+
+  // render() {
+  // }
+}
+
+export default MovieLibrary;
