21a820da9043dffc16e5033a9d1e0384b6a7b58e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-18 10:06:26 -0300

    function filtering movies ready

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -13,8 +13,9 @@ class MovieLibrary extends Component {
       movies: this.props.movies,
     };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
-    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
   }
 
   onSearchTextChange(event) {
@@ -32,6 +33,29 @@ class MovieLibrary extends Component {
     this.setState({ bookmarkedOnly: checked });
   }
 
+  filterMovies() {
+    const { selectedGenre, movies, searchText, bookmarkedOnly } = this.state;
+    // tem que repetir o destruturing pois n parece pegar de cima
+    // Quando estado bookmarkedOnly true, renderizar fav;
+    if (bookmarkedOnly === true) {
+      const favMovies = movies.filter((mov) => mov.bookmarked);
+      return favMovies;
+    }
+    // Quando estado selectedGenre não vazio, renderizar filmes deste gênero;
+    if (selectedGenre.length !== 0) {
+      const moviesByGenre = movies.filter((mov) => mov.genre === selectedGenre);
+      return moviesByGenre;
+    }
+    // Quando o estado searchText não vazio, renderizar filmes com texto;
+    const movieByText = movies.filter((mov) => (
+      (mov.title.includes(searchText))
+      || (mov.subtitle.includes(searchText))
+      || (mov.storyline.includes(searchText))
+    ));
+    return movieByText;
+    // Caso contrario para todos esses ifs, simplesmente passa todos filmes
+  }
+
   render() {
     return (
       <div>
@@ -44,7 +68,7 @@ class MovieLibrary extends Component {
           selectedGenre={this.state.selectedGenre}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={this.filterMovies()} />
       </div>
     );
   }
