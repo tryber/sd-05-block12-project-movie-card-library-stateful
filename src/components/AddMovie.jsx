9c6d56be488a44f1c2fa6b466298d21e00ba1e96
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-18 17:38:36 -0300

    AddMovie component implemented - codeclimate issues fixed

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,6 +12,8 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
     this.changeInputValue = this.changeInputValue.bind(this);
+    this.changeInputRatingValue = this.changeInputRatingValue.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   onClick() {
@@ -42,22 +44,22 @@ class AddMovie extends React.Component {
       <section>
         <form>
           <label htmlFor="title"> Título
-            <input type="text" name="title" value={this.state.title} onChange={this.changeInputValue} />
+            <input type="text" value={this.state.title} onChange={this.changeInputValue} />
           </label>
           <label htmlFor="subtitle"> Subtítulo
-            <input type="text" name="subtitle" value={this.state.subtitle} onChange={this.changeInputValue} />
+            <input type="text" value={this.state.subtitle} onChange={this.changeInputValue} />
           </label>
           <label htmlFor="image"> Imagem
-            <input type="text" name="image" value={this.state.imagePath} onChange={this.changeInputValue} />
+            <input type="text" value={this.state.imagePath} onChange={this.changeInputValue} />
           </label>
           <label htmlFor="storyline"> Sinopse
-            <textarea type="text" name="storyline" value={this.state.storyline} onChange={this.changeInputValue} />
+            <textarea type="text" value={this.state.storyline} onChange={this.changeInputValue} />
           </label>
           <label htmlFor="rating"> Rating
-            <input type="number" name="rating" value={this.state.rating} onChange={this.changeInputRatingValue} />
+            <input type="number" value={this.state.rating} onChange={this.changeInputRatingValue} />
           </label>
           <label htmlFor="genre"> Gênero
-            <select name="genre" value={this.state.genre} onChange={this.changeInputValue}>
+            <select value={this.state.genre} onChange={this.changeInputValue}>
               <option value="action">Ação</option>
               <option value="comedy">Comédia</option>
               <option value="thriller">Suspense</option>
