6039182d06acfb47b3f6780d52724a7494d56a61
Lucas Staroscky <LucasStaroscky@MBP-de-Lucas.lan>
2020-07-18 17:27:59 -0300

    AddMovie component implemented - codeclimate issues fixed

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,25 +2,20 @@ import React from 'react';
 
 class AddMovie extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       subtitle: '',
       title: '',
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
-    }
+      genre: 'action',
+    };
     this.changeInputValue = this.changeInputValue.bind(this);
   }
 
-  changeInputValue(e) {
-    let { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
-
   changeInputRatingValue(e) {
-    let { name, num } = e.target;
+    const { name, num } = e.target;
     this.setState({ [name]: Number(num) });
   }
 
@@ -37,36 +32,41 @@ class AddMovie extends React.Component {
     });
   }
 
+  changeInputValue(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <section>
         <form>
-          <label>
+          <label htmlFor="title">
             Título
             <input
               type="text" name="title" value={this.state.title} onChange={this.changeInputValue} />
           </label>
-          <label>
+          <label htmlFor="subtitle">
             Subtítulo
             <input
               type="text" name="subtitle" value={this.state.subtitle} onChange={this.changeInputValue} />
           </label>
-          <label>
+          <label htmlFor="image">
             Imagem
             <input
               type="text" name="image" value={this.state.imagePath} onChange={this.changeInputValue} />
           </label>
-          <label>
+          <label htmlFor="storyline">
             Sinopse
             <textarea
               type="text" name="storyline" value={this.state.storyline} onChange={this.changeInputValue} />
           </label>
-          <label>
+          <label htmlFor="rating">
             Rating
             <input
               type="number" name="rating" value={this.state.rating} onChange={this.changeInputRatingValue} />
           </label>
-          <label>
+          <label htmlFor="genre">
             Gênero
             <select
               name="genre" value={this.state.genre} onChange={this.changeInputValue}>
@@ -75,10 +75,10 @@ class AddMovie extends React.Component {
               <option value="thriller">Suspense</option>
             </select>
           </label>
-          <button onClick={this.onClick}>Adicionar filme</button>
+          <button type="button" onClick={this.onClick}>Adicionar filme</button>
         </form>
       </section>
-    )
+    );
   }
 }
 
