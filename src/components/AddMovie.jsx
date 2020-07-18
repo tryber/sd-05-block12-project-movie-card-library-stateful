337681f8d1eda3922c0e0002b91969576f3833f4
Marylange <marylangess@gmail.com>
2020-07-18 17:38:32 -0300

    verificando requisitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,13 +12,37 @@ class AddMovie extends React.Component {
       genre: 'action',
     };
   }
-
+  setFilterValue = event => {
+    this.setState({
+      title: event.target.value,
+      subtitle: event.target.value,
+      imagePath: event.target.value,
+      storyline: event.target.value,
+      rating: event.target.value,
+      genre: event.target.value,
+    })
+  }
   render() {
     return (
       <div>
         <form>
           <label htmlFor="title">Título</label>
-          <input id="title" />
+          <input type="text" id="title" onChange={this.setFilterValue.title} />
+          <label htmlFor="subtitle">Subtítulo</label>
+          <input type="text" id="subtitle" onChange={this.setFilterValue.subtitle} />
+          <label htmlFor="image">Imagem</label>
+          <input type="text" id="image" onChange={this.setFilterValue.imagePath} />
+          <label htmlFor="synopsis">Sinopse</label>
+          <textarea id="synopsis" onChange={this.setFilterValue.storyline}></textarea>
+          <label htmlFor="number">Avaliação</label>
+          <input type="number" id="number" onChange={this.setFilterValue.rating} />
+          <label htmlFor="gender">Gênero</label>
+          <select id="gender" onChange={this.setFilterValue.genre}>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
+          <button>Adicionar filme</button>
         </form>
       </div>
     );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,11 +18,11 @@ class SearchBar extends React.Component {
       <div>
         <form>
           <label htmlFor="includeText">Inclui o texto:</label>
-          <input type="text"id="includeText"value={searchText} onChange={this.onSearchTextChange} />
-          <input type="checkbox" id="favorites" checked={bookmarkedOnly} onChange={this.onBookmarkedChange} />
+          <input type="text"id="includeText"value={searchText} onChange={onSearchTextChange} />
+          <input type="checkbox"id="favorite"checked={bookmarkedOnly}onChange={onBookmarkedChange} />
           <label htmlFor="favorites">Mostrar somente favoritos</label>
           <label htmlFor="filterGender">Filtrar por gênero</label>
-          <select value={selectedGenre}id="filterGender"onChange={this.onSelectedGenreChange}>
+          <select value={selectedGenre}id="filterGender"onChange={onSelectedGenreChange}>
             <option value="">Todos</option>
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
