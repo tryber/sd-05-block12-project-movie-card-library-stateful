732baac88dda33db43b78de4deecb535d30a9449
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-19 16:04:07 -0300

    codeclimate 10

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,14 +5,14 @@ import SearchBar from './SearchBar';
 // import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
- 
+
   render() {
     return (
       <div>
         <h2>My awsome movie library</h2>
         <SearchBar />
         <MovieList movies={this.props.movies} />
-        <AddMovie />
+        {/* <AddMovie /> */}
       </div>
     );
   }
