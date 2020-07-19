a59f7d99660980f1f192b34fbd0aa9a1f242d50c
Mau <mauricio.sdjr@gmail.com>
2020-07-19 13:10:09 -0300

    codeclimate and add bind

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import InputText from './InputText';
 import TextArea from './TextArea';
 import InputNumber from './InputNumber';
+import Select from './Select';
 
 class addMovie extends React.Component {
   constructor(props) {
@@ -15,30 +16,30 @@ class addMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.changeHandler = this.changeHandler.bind(this);
+    this.numChangeHandler = this.numChangeHandler.bind(this);
   }
 
-  changeHandler = (event) => {
+  changeHandler(event) {
     const { name, value } = event.target;
-    this.setState({ 
-      [name]: value 
-    })
+    this.setState({ [name]: value });
   }
 
-  numChangeHandler = (event) => {
+  numChangeHandler(event) {
     const { name, value } = event.target;
-    this.setState({
-      [name]: parseFloat(value)
-    })
+    this.setState({ [name]: parseFloat(value) });
   }
 
   render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
-        <InputText id="title" texto="Título" e={this.changeHandler} value={this.state.title} />
-        <InputText id="subtitle" texto="Subtítulo" e={this.changeHandler} value={this.state.subtitle} />
-        <InputText id="imagePath" texto="Imagem" e={this.changeHandler} value={this.state.imagePath} />
-        <TextArea id="storyline" texto="Sinopse" e={this.changeHandler} value={this.state.storyline} />
-        <InputNumber id="rating" texto="Avaliação" e={this.numChangeHandler} value={this.state.rating} />
+        <InputText id="title" texto="Título" e={this.changeHandler} value={title} />
+        <InputText id="subtitle" texto="Subtítulo" e={this.changeHandler} value={subtitle} />
+        <InputText id="imagePath" texto="Imagem" e={this.changeHandler} value={imagePath} />
+        <TextArea id="storyline" texto="Sinopse" e={this.changeHandler} value={storyline} />
+        <InputNumber id="rating" texto="Avaliação" e={this.numChangeHandler} value={rating} />
+        <Select id="genre" texto="Gênero" e={this.changeHandler} value={genre} />
       </form>
     );
   }
