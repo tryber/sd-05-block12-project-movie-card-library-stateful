b5246a848a686c12156d43942d464ca93fda81cb
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-07-19 18:33:19 -0300

    requirement AddMovie implemented

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -6,7 +6,6 @@ class SelectBox extends React.Component {
       <label htmlFor={this.props.label}>
         {this.props.topic}
         <select name={this.props.label} value={this.props.value} onChange={this.props.change}>
-          <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
