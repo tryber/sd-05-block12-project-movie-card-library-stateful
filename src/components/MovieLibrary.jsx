0a1cdc317f6e9f3ea89ed70236debe2ce4aa770b
Preto115 <preto115@gmail.com>
2020-07-20 13:18:43 -0300

    requesito MovieLibrary parcialmente implementado

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
+// import LocalStorage from './LocalStorage';
 // import movies from '../data';
 // import movies from '../data';
 
@@ -17,15 +18,15 @@ class MovieLibrary extends React.Component {
       selectedGenre: '',
       movies: [...movies],
     };
-    // console.log(this.state);
+    console.log(this.state.movies);
     this.cadastraMovie = this.cadastraMovie.bind(this);
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.filtraSaida = this.filtraSaida.bind(this);
   }
 
   onSearchTextChange(event) {
-    // const { name, value } = event.target;
     this.setState({ searchText: event.target.value });
   }
 
@@ -34,20 +35,48 @@ class MovieLibrary extends React.Component {
   }
 
   onBookmarkedChange(event) {
-    this.setState({ bookmarkedOnly: event.target.value });
+    this.setState({ bookmarkedOnly: event.target.checked });
   }
 
   cadastraMovie(filme) {
     const { movies } = this.props;
     this.setState({ movies: movies.push(filme) });
-  // this.setState(async () => ({ filmes: await [filme] }))
+    // console.log(this.state.movies);
   }
 
+  filtraSaida(lista) {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    if (bookmarkedOnly === true) {
+      return lista.filter((filme) => filme.bookmarked === bookmarkedOnly);
+    }
+    if (selectedGenre !== '') {
+      return lista.filter((filme) => filme.genre === selectedGenre);
+    }
+    if (searchText !== '') {
+      return lista.filter((filme) => filme.title.includes(searchText)
+      || filme.subtitle.includes(searchText)
+      || filme.storyline.includes(searchText));
+    }
+    // console.log(resultado);
+    return lista;
+  }
+
+  // {
+  //   title: 'Resident Evil',
+  //   subtitle: 'Vendetta',
+  //   storyline: 'Chris Redfield enlists the help of Leon S.
+  // Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance,
+  // from spreading a deadly virus in New York.',
+  //   rating: 4.2,
+  //   imagePath: 'images/Resident_Evil_Vendetta.jpg',
+  //   bookmarked: true,
+  //   genre: 'fantasy',
+  // },
   render() {
     const { searchText, selectedGenre, bookmarkedOnly } = this.state;
     const { movies } = this.props;
     // console.log(this.state.filmes);
-
+    // this.setState(LocalStorage(this.sate));
     return (
       <div>
         <SearchBar
@@ -58,7 +87,7 @@ class MovieLibrary extends React.Component {
           selectedGenre={selectedGenre}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={movies} />
+        <MovieList movies={this.filtraSaida(movies)} />
         <AddMovie onClick={this.cadastraMovie} />
       </div>
     );
