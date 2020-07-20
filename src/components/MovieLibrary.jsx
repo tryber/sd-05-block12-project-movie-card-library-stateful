62bad8e88cd982eea65d9c04a426063c6ebae74c
luca <luucadecastro@gmail.com>
2020-07-20 13:34:08 -0300

    2o cc

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,7 +14,7 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.mvoies} />
+        <MovieList movies={this.props.movies} />
         <AddMovie />
       </div>
     );
