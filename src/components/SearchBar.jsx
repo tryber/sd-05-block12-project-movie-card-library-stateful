dc82f8f26d89d3b9d51bd695a06a25a64385a78d
Andrevlc <andrevezi@gmail.com>
2020-07-21 19:50:31 -0300

    Commit 10 - Fixed checkbox and added genre filter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const space = '  ';
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
-    const { onBookmarkedChange } = this.props;
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
+    } = this.props;
     return (
       <div className="class-list">
         <form>
@@ -14,6 +16,14 @@ class SearchBar extends React.Component {
           <label htmlFor="favorite">Mostrar somente favoritos {space}
             <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
           </label><br />
+          <label htmlFor='genre'>Filtrar por gênero {space}
+            <select name='genre' value={selectedGenre} onChange={onSelectedGenreChange}>
+              <option value=''>Todos</option>
+              <option value='action'>Ação</option>
+              <option value='comedy'>Comédia</option>
+              <option value='thriller'>Suspense</option>
+            </select>
+          </label>
         </form>
       </div>
     );
