351a680c30880d146e534816929acec0e0ab7f99
Andrevlc <andrevezi@gmail.com>
2020-07-21 17:58:31 -0300

    Commit 5 - added addmovie.jsx

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -16,16 +16,19 @@ class AddMovie extends React.Component {
       storyline: '',
       rating: 0,
       genre: 'action',
-    }
+    };
     this.eventStr = this.eventStr.bind(this);
     this.eventNmb = this.eventNmb.bind(this);
     this.ClearMovie = this.ClearMovie.bind(this);
   }
-  eventStr({ target: { value } }, name) {
-    return this.setState({ [name]: value });
+  eventStr(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
-  eventNmb({ target: { value } }, name) {
-      return this.setState({ [name]: Number(value) });
+
+  eventNmb(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: Number(value) });
   }
   ClearMovie() {
     const onClick = this.props.onClick;
@@ -40,34 +43,27 @@ class AddMovie extends React.Component {
     });
   }
   render() {
-    const { title, subtitle, imagePath, storyline, rating, genre} = this.state
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
-        <label htmlFor='title'>Título
-          <input type='text' onChange={(event) => this.eventStr(event, 'title')} value={title}/>
-        </label><br></br>
-        <label htmlFor='subtitle'>Subtítulo
-          <input type='text' onChange={(event) => this.eventStr(event, 'subtitle')} value={subtitle}/>
-        </label><br></br>
-        <label htmlFor='imagePath'>Imagem
-          <input type='text' onChange={(event) => this.eventStr(event, 'imagePath')} value={imagePath}/>
-        </label><br></br>
-        <label htmlFor='storyline'>Sinopse
-          <textarea type='text' onChange={(event) => this.eventStr(event, 'storyline')} value={storyline}/>
-        </label><br></br>
-        <label htmlFor='rating'>Avaliação
-          <input type='number' onChange={(event) => this.eventNmb(event, 'rating')} value={rating}/>
-        </label><br></br>
-        <label htmlFor='genre'>Gênero
-          <select name='genre'onChange={(event) => this.eventStr(event, 'genre')} value={genre}>
-            <option value='action' key='action'>{movieGenres[0].innerText}</option>
-            <option value='comedy' key='comedy'>{movieGenres[1].innerText}</option>
-            <option value='thriller' key='thriller'>{movieGenres[2].innerText}</option>
+        <label htmlFor="title">Título
+          <input type="text" onChange={this.eventStr} value={title} /></label><br />
+        <label htmlFor="subtitle">Subtítulo
+          <input type="text" onChange={this.eventStr} value={subtitle} /></label><br />
+        <label htmlFor="imagePath">Imagem
+          <input type="text" onChange={this.eventStr} value={imagePath} /></label><br />
+        <label htmlFor="storyline">Sinopse
+          <textarea type="text" onChange={this.eventStr} value={storyline} /></label><br />
+        <label htmlFor="rating">Avaliação
+          <input type="number" onChange={this.eventNmb} value={rating} /></label><br />
+        <label htmlFor="genre">Gênero
+          <select name="genre"onChange={this.eventStr} value={genre}>
+            <option value="action" key="action">{movieGenres[0].innerText}</option>
+            <option value="comedy" key="comedy">{movieGenres[1].innerText}</option>
+            <option value="thriller" key="thriller">{movieGenres[2].innerText}</option>
           </select>
-        </label>
-        <button type="button" onClick={this.ClearMovie}>
-          Adicionar filme
-        </button>
+        </label><br />
+        <button type="button" onClick={this.ClearMovie}>Adicionar filme</button>
       </form>
     );
   }
