74809536e75f0ae0b28ac7bc743c8dd7e9823259
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-21 19:08:45 -0300

    codeclimate adjusts

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.props.movies
-    }
+      movies: this.props.movies,
+    };
   }
   render() {
     return (
       <SearchBar />
-    )
+    );
   }
 }
 
