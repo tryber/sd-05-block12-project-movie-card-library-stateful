7513070af2d29e19b9fd619a39bed4f93e2f95db
Andrevlc <andrevezi@gmail.com>
2020-07-21 19:28:23 -0300

    Commit 8 CC fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 const space = '  ';
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
-    const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <div className='class-list'>
+      <div className="class-list">
         <form>
-          <label htmlFor='incText'>Inclui o texto {space}
-            <input type='text' value={searchText} onChange={onSearchTextChange} />
+          <label htmlFor="incText">Inclui o texto {space}
+            <input type="text" value={searchText} onChange={onSearchTextChange} />
           </label><br />
-       '</form>
+        </form>
       </div>
     );
   }
