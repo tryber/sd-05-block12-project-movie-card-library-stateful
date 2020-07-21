22e1ed93da9a1a8ba774c0a22337ffe3fb1b626c
labelle <beelsdelonge@gmail.com>
2020-07-20 22:13:05 -0300

    AddMovie quase completo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,16 +1,116 @@
-// implement AddMovie component here
 import React from 'react';
 
 const Input = (props) => {
-  const { type, id, onChange, name, value } = props;
+  const { type, key, onChange, name, value } = props;
   return (
     <input
       type={type}
-      id={id}
+      key={key}
       onChange={onChange}
       name={name}
       value={value}
     />
+  );
+};
+
+const Titulo = (props) => {
+  const { value, onChange } = props;
+  return (
+    <label htmlFor="label1">Título
+      <Input
+        name="title"
+        type="text"
+        key="label1"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+const Subtitulo = (props) => {
+  const { value, onChange } = props;
+  return (
+    <label htmlFor="label2">Subtítulo
+      <Input
+        name="subtitle"
+        type="text"
+        key="label2"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+const Imagem = (props) => {
+  const { value, onChange } = props;
+  return (
+    <label htmlFor="label3">Imagem
+      <Input
+        name="imagePath"
+        type="text"
+        key="label3"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+const Sinopse = (props) => {
+  const { value, onChange } = props;
+  return (
+    <label htmlFor="label4">Sinopse
+      <textarea
+        name="storyline"
+        type="textarea"
+        key="label4"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+const Avaliacao = (props) => {
+  const { value, onChange } = props;
+  return (
+    <label htmlFor="label5">Avaliação
+      <Input
+        name="rating"
+        type="number"
+        key="label5"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+const Genero = (props) => {
+  const { value, onChange } = props;
+  return (
+    <label htmlFor="label6">Gênero
+      <select
+        value={value}
+        onChange={onChange}
+        key="label6"
+      >
+        <option value="action">Ação</option>
+        <option value="comedy">Comédia</option>
+        <option value="thriller">Suspense</option>
+      </select>
+    </label>
+  );
+};
+
+const NewMovie = (props) => {
+  const { resetState } = props;
+  return (
+    <div>
+      <button type="button" onClick={() => resetState()}>Adicionar filme</button>
+    </div>
   );
 };
 
@@ -20,76 +120,45 @@ class AddMovie extends React.Component {
     this.state = { subtitle: '', title: '', imagePath: '', storyline: '', rating: 0, genre: 'action' };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.resetState = this.resetState.bind(this);
   }
 
   handleChange(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: [value] });
+    this.setState({ [name]: value });
   }
 
-  renderInputs() {
-    const { title, subtitle } = this.state;
-    return (
-      <div>
-        <label htmlFor="label1">Título</label>
-        <Input
-          name="title"
-          type="text"
-          value={title}
-          onChange={(e) => this.handleChange(e)}
-        />
-        <label htmlFor="label2">Subtítulo</label>
-        <Input
-          name="subtitle"
-          type="text"
-          value={subtitle}
-          onChange={(e) => this.handleChange(e)}
-        />
-      </div>
-    );
+  handleChange2(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: Number(value) });
   }
 
-  renderInputs2() {
-    const { imagePath, storyline, rating } = this.state;
-    return (
-      <div>
-        <label htmlFor="label3">Imagem</label>
-        <Input
-          name="imagePath"
-          type="file"
-          value={imagePath}
-          onChange={(e) => this.handleChange(e)}
-        />
-        <label htmlFor="label4">Sinopse</label>
-        <Input
-          name="sinopse"
-          type="textarea"
-          value={storyline}
-          onChange={(e) => this.handleChange(e)}
-        />
-        <label htmlFor="label5">Avaliação</label>
-        <Input name="rating" type="number" value={rating} onChange={(e) => this.handleChange(e)} />
-      </div>
-    );
+  resetState() {
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
   }
 
   render() {
-    const { genre } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
-        {this.renderInputs()}
-        {this.renderInputs2()}
-        <label htmlFor="label6">Gênero</label>
-        <select value={genre} onChange={(e) => this.handleChange(e)} id="label6">
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
-        </select>
-        <button type="submit">Adicionar filme</button>
+        <Titulo value={title} onChange={this.handleChange} />
+        <Subtitulo value={subtitle} onChange={this.handleChange} />
+        <Imagem value={imagePath} onChange={this.handleChange} />
+        <Sinopse value={storyline} onChange={this.handleChange} />
+        <Avaliacao value={rating} onChange={this.handleChange2} />
+        <Genero value={genre} onChange={this.handleChange} />
+        <NewMovie resetState={this.resetState} />
       </form>
     );
   }
 }
 
 export default AddMovie;
-
