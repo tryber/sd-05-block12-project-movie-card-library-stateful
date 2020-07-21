63d84e15de7de7fb34a5966005b5b0c5f3569140
hleoc <informig@gmail.com>
2020-07-21 13:58:25 -0300

    Segundo commit

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,63 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.change = this.change.bind(this);
+  }
+
+  change(event) {
+    const e = event.target;
+    this.setState({ [e.id]: e.id === 'rating' ? parseFloat(e.value) : e.value });
+  }
+
+  buttonChange() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="title">Título</label>
+        <input id="title" type="text" value={this.state.title} onChange={this.change} />
+        <label htmlFor="subtitle">Subtítulo</label>
+        <input id="subtitle" type="text" value={this.state.subtitle} onChange={this.change} />
+        <label htmlFor="imagePath">Imagem</label>
+        <input
+          id="imagePath" type="text" value={this.state.imagePath}
+          onChange={this.change}
+        />
+        <label htmlFor="storyline">Sinopse</label>
+        <textarea id="storyline" value={this.state.storyline} onChange={this.change} />
+        <label htmlFor="rating">Avaliação</label>
+        <input id="rating" type="number" value={this.state.rating} onChange={this.change} />
+        <label htmlFor="genre">Gênero</label>
+        <select id="genre" value={this.state.genre} onChange={this.change}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+        <button onClick={this.buttonChange}>Adicionar filme</button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-
+// HUGO
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState(event) {
+    this.setState({
+      searchText: event.target.value
+      
+    })
   }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar searchText={this.state.searchText} onSearchTextChange={this.changeState} bookmarkedOnly={this.state.changeState} />
         <MovieList movies={this.props.movies} />
+      
         <AddMovie />
       </div>
     );
