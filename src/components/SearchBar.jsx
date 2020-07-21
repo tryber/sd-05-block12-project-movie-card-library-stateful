dfa24bc755c3bf4f6f3af22c6cb492be76cd2ac4
Andrevlc <andrevezi@gmail.com>
2020-07-21 18:05:23 -0300

    Commit 5.1 - fixed CC of SearcBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor(props){
-    super(props)
-    this.state ={
-      filter:'all'
-    }
+  constructor(props) {
+    super(props);
+    this.state = { filter: 'all' };
   }
   // setValueFilter = event => {
   //   this.setState({
   //     filter:event.target.value == '' ?  'all' : event.target.value
-  //     }) 
+  //     })
   //  }
 
   render() {
     return (
       <div className="movie-list">
-        <input onChange={this.setValueFilter} type='text' />
+        <input onChange={this.setValueFilter} type="text" />
       </div>
     );
   }
