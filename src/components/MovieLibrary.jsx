a8babbe06e8d5f42ba292d73dee348c8ab6f93b9
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-21 19:06:15 -0300

    MovieLibrary in progress

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,21 @@
-// code here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies
+    }
+  }
+  render() {
+    return (
+      <SearchBar />
+    )
+  }
+}
+
+export default MovieLibrary;
