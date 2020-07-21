2c2dbccd4a6e3f8911e436efb4122d2cc8fced00
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-20 21:15:54 -0300

    AddMovie component created

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,32 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+      imagePath: '',
+    }
+  }
+
+  searchTitle = (element) => {
+    const { name, value } = element.target;
+    this.setState({[name]: value})
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Título<input onChange={this.searchTitle} value={this.state.title} type="text" /></label>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AddMovie;
