fe31577f3c6f8437a65cfed248b8c39e4629970c
Andrevlc <andrevezi@gmail.com>
2020-07-21 18:30:46 -0300

    Commit 6 - starting search bar - first requisites

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,16 +5,18 @@ class SearchBar extends React.Component {
     super(props);
     this.state = { filter: 'all' };
   }
-  // setValueFilter = event => {
-  //   this.setState({
-  //     filter:event.target.value == '' ?  'all' : event.target.value
-  //     })
-  //  }
+  setValueFilter(event) {
+    this.setState({ filter: event.target.value === '' ? 'all' : event.target.value });
+  }
 
   render() {
     return (
       <div className="movie-list">
-        <input onChange={this.setValueFilter} type="text" />
+        <form>
+          <label htmlFor="incText">Inclui o texto
+            <input onChange={this.setValueFilter} type="text" />
+          </label>
+        </form>
       </div>
     );
   }
