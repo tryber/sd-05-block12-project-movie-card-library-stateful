01d7bdfcbc11ef0ddb2729b5058c4648b8a5e177
luialbeto <araujo.script@gmail.com>
2020-07-22 12:35:34 -0300

    climate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,6 @@
-// implement AddMovie component here
-class AddMovie extends React.Component {
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,10 +11,18 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
-    this.change = this.change.bind(this);
+    this.modificandoEstado = this.modificandoEstado.bind(this);
+    this.buttonChange = this.buttonChange.bind(this);
+  }
+
+  modificandoEstado(event) {
+    const { name, value } = event.target;
+    this.state({ [name]: name === 'rating' ? parseFloat(value) : value });
   }
 
   buttonChange() {
+    const { onClick } = this.props;
+    onClick(this.state);
       this.setState({
       subtitle: '',
       title: '',
@@ -25,24 +34,26 @@ class AddMovie extends React.Component {
   }
 
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form>
-        <label>Título</label>
-        <label>Subtítulo</label>
-        <input>
-        <label>Imagem</label>
-        <input/>
-        <label>Sinopse</label>
-        <textarea/>
-        <label>Avaliação</label>
-        <input/>
-        <label>Gênero</label>
-        <select>
-          <option>Ação</option>
-          <option>Comédia</option>
-          <option>Suspense</option>
+        <label htmlFor="title">Título</label>
+        <input type="text" value={title} name="title" onChange={this.modificandoEstado} />
+        <label htmlFor="subtitle">Subtítulo</label>
+        <input type="text" value={subtitle} name="subtitle" onChange={this.modificandoEstado} />
+        <label htmlFor="imagePath">Imagem</label>
+        <input type="text" value={imagePath} name="imagePath" onChange={this.modificandoEstado} />
+        <label htmlFor="storyline">Sinopse</label>
+        <textarea name="storyline" value={storyline} onChange={this.modificandoEstado} />
+        <label htmlFor="rating">Avaliação</label>
+        <input name="rating" value={rating} onChange={this.modificandoEstado} />
+        <label htmlFor="genre">Gênero</label>
+        <select name="genre" value={genre} onChange={this.modificandoEstado}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
         </select>
-        <button>Adicionar filme</button>
+        <button type="button" onClick={this.buttonChange}>Adicionar filme</button>
       </form>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,33 @@
 // implement SearchBar component here
-import React from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component {
   render() {
-    const searchBar = () => (
-      <SearchBar
-        searchText={props.searchText}
-        onSearchTextChange={props.onSearchTextChange}
-        bookmarkedOnly={props.bookmarkedOnly}
-        onBookmarkedChange={props.onBookmarkedChange}
-        selectedGenre={props.selectedGenre}
-        onSelectedGenreChange={props.onSelectedGenreChange}
-        />,
-    )
-  }
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
   
-  return (
-    <form>
-      <label htmlFor="texto">Inclui o texto: </label>
-      <input id="texto" type="text" value={searchText} onChange={onSearchTextChange} />
-      <label htmlFor="check">Mostrar somente favoritos</label>
-      <input id="check" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-      <label htmlFor="selec">Filtrar por gênero</label>
-      <select id="selec" value={selectedGenre} onChange={onSelectedGenreChange}>
-        <option value="">Todos</option>
-        <option value="action">Ação</option>
-        <option value="comedy">Comédia</option>
-        <option value="thriller">Suspense</option>
-      </select>
-    </form>
-  );
+    return (
+      <form>
+        <label htmlFor="searchText">Inclui o texto: </label>
+        <input name ="searchText" type="text" value={searchText} onChange=    {onSearchTextChange} />
+        <label htmlFor="bookmarkedOnly">Mostrar somente favoritos</label>
+        <input type="checkbox" name='bookmarkedOnly' checked={bookmarkedOnly}     onChange={onBookmarkedChange} />
+        <label htmlFor="selec">Filtrar por gênero</label>
+        <select value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
