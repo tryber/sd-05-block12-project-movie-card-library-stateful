aebf2b45e903eac85a2db0e8b005984a969b8a43
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-22 11:37:03 -0300

    descomentando MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,29 +1,50 @@
-// // implement MovieLibrary component here
-// import React, { Component } from 'react';
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// // import AddMovie from './AddMovie';
+// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
-// class MovieLibrary extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       searchText: '',
-//       bookmarkedOnly: false,
-//       selectedGenre: '',
-//       movies: this.props,
-//     };
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.filteredMovie = this.filteredMovie.bind(this);
+    const movie = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movie,
+    };
+  }
 
-//     render();
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         {/* <AddMovie /> */}
-//       </div>
-//     );
-//   }
-// }
+  filteredMovie() {
+    const { selectedGenre, movies, searchText, bookmarkedOnly } = this.state;
 
-// export default MovieLibrary;
+    if (bookmarkedOnly) return movies.filter((movie) => movie.bookmarked);
+    if (selectedGenre.length) return movies.filter((movie) => movie.genre === selectedGenre);
+
+    return movies.filter((movie) => (
+      (movie.title.includes(searchText))
+      || (movie.subtitle.includes(searchText))
+      || (movie.storyline.includes(searchText))
+    ));
+  }
+
+
+  render() {
+    return (
+      <section>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={(event) => { this.setState({ searchText: event.target.value }); }}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={(ev) => { this.setState({ bookmarkedOnly: ev.target.checked }); }}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={(ev) => { this.setState({ selectedGenre: ev.target.value }); }}
+        />
+        <MovieList movie={this.filteredMovie} />
+      </section>
+    );
+  }
+}
+
+export default MovieLibrary;
