614614d65b09984b6e63c44e89c08f75634d54ac
pauloricardoz <trybe.przt@gmail.com>
2020-07-22 01:26:16 -0300

    codeclime plus top galaxy vol 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
-
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLibrary movies={movies}/>
+      <MovieLibrary movies={movies} />
     </div>
   );
 }
