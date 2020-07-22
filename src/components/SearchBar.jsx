f3f9b982f4c0726c021e50b9f3fe0089dd8812fb
César Alves <alvescesar@hotmail.com>
2020-07-21 22:20:56 -0300

    Added 'SearchBar'as functional component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,32 @@
-// implement SearchBar component here
+import React from 'react';
+
+function SearchBar(props) {
+  const {
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
+  } = props;
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="include-text">Inclui o texto: </label>
+        <input type="text" id="include-text" value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor="favorites">Mostrar somente favoritos</label>
+        <input type="checkbox" id="favorites" value={bookmarkedOnly} onChange={onBookmarkedChange} />
+        <label htmlFor="filter-by-gender">Filtrar por gênero</label>
+        <select id="filter-by-gender" value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
