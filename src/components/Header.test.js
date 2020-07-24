2f3c941cb6694ae7e7f22df590e98e1587d3c7c7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-24 18:02:02 -0300

    Removing .skip from Header.

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -4,11 +4,11 @@ import { shallow } from 'enzyme';
 import Header from './Header';
 
 describe('<Header /> component', () => {
-  it.skip('renders without crashing', () => {
+  it('renders without crashing', () => {
     shallow(<Header />);
   });
 
-  it.skip('it includes the text `Movie Cards Library` inside a h1 tag', () => {
+  it('it includes the text `Movie Cards Library` inside a h1 tag', () => {
     const wrapper = shallow(<Header />);
 
     expect(wrapper.find('header h1').text()).toBe('Movie Cards Library');
