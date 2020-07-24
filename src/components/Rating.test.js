ec03ab4951dd5f008813f620f14db86c2a93c65d
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-24 18:06:51 -0300

    Removing .skip from Rating.

--- src/components/Rating.test.js
+++ src/components/Rating.test.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { shallow } from 'enzyme';
 
 import Rating from './Rating';
 
 describe('<Rating /> component', () => {
-  it.skip('renders without crashing', () => {
+  it('renders without crashing', () => {
     shallow(<Rating />);
   });
 
-  it.skip('renders the rating inside an element with the class `rating`', () => {
+  it('renders the rating inside an element with the class `rating`', () => {
     const wrapper = shallow(<Rating rating={3} />);
 
     expect(wrapper.find('.rating').text()).toEqual('3');
