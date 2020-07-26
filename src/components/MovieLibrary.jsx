bb50201df4a354d2d1882e3bb476ec0f520f5b88
Sidnei Ramos <sidneiramos02@gmail.com>
2020-07-26 11:31:53 -0300

    Initial Commit 2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -14,9 +14,9 @@ class MovieLibrary extends Component {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        {/* <SearchBar /> */}
+        {/* <MovieList movies={this.props.movies} /> */}
+        {/* <AddMovie /> */}
       </div>
     );
   }
