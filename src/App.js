7b3efb3582cc5f116838e6b598d647838d266674
Erlingsjunior <erlingsjunior@gmail.com>
2020-08-03 00:48:30 -0300

    searchBar bookMarkedOnly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import Movies from './data';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
        componente eu a verdade
        estou importando seu endereço */}
       <Header />
+      <SearchBar />
       <MovieList movies={Movies} />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,19 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    // descontrução de objeto
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return(
+      <form>
+        <label htmlFor="find">Inclui o texto: </label>
+        <input type="text" name="find" value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor="find2">Mostrar somente favoritos</label>
+        <input type="checkbox" name="find2" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
