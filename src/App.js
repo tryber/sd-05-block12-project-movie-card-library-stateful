d662d36757748480b7bb576b2f3447ed6105319b
fabiano <fasnaweb2020@gmail.com>
2020-08-03 16:13:41 -0300

    Primeiros Requisitos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
-
+import SearchBar from './components/SearchBar';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      texto: ''
+    }
+  }
+
+  onSearchTextChange() {
+    return 'teste';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar searchText="Digite aqui" func={this.onSearchTextChange}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
-// implement SearchBar component here
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Inclui o texto:
+            <br />
+           <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}/>
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
