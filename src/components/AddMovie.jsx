b107b757ca94cd06742861a4b2f1088cd46e7730
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-02 22:41:03 -0300

    Adicionado alguns componentes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,15 @@
 // implement AddMovie component here
+import React, { Component } from 'react'
+
+export default class AddMovie extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
