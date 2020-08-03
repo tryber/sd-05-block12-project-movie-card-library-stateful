b0fda35fc98221f82c16c90b0f039026cda76915
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-02 21:50:47 -0300

    Correçoes do Climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,30 +4,24 @@ import React from 'react';
 export default class SearchBar extends React.Component {
   render() {
     const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
+      searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
       <form>
-        <label>Inclui o texto</label>
+        <label htmlFor='a'>Inclui o texto</label>
         <input type="text" value={searchText} onChange={onSearchTextChange} />
         <input
           type="checkbox"
           checked={bookmarkedOnly}
           onChange={onBookmarkedChange}
         />
-        <label>Mostrar somente favoritos</label>
+        <label htmlFor='b'>Mostrar somente favoritos</label>
         <select value={selectedGenre} onChange={onSelectedGenreChange}>
-          <option value="">Todos</option>
-          <option value="action">Ação</option>
-          <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>
+          <option value="">Todos</option><option value="action">Ação</option><option value="comedy">Comédia</option><option value="thriller">Suspense</option>
         </select>
-        <label>Filtrar por gênero</label>
+        <label htmlFor='c'>Filtrar por gênero</label>
       </form>
     );
   }
