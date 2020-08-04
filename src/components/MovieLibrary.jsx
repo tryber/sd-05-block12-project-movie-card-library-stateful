66ca10569ff9b63760d0f6d1e5b9ffbcaf7377dc
Larissa Palombo <larissampalombo@gmail.com>
2020-08-04 17:53:58 -0300

    só falta movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
+class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.movies,
+      movies: this.props.movies,
     };
   }
 }
