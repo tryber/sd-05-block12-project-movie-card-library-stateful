a37699cb6440e2db0d4f790196c65ec4b568e804
Fas-fCode <fasnaweb2020@gmail.com>
2020-08-14 20:44:41 -0300

    SearchBar  ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React from "react";
 
 export default (props) => {
   const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
+    searchText,
+    onSearchTextChange,
+    bookmarkedOnly,
+    onBookmarkedChange,
+    selectedGenre,
+    onSelectedGenreChange,
   } = props;
 
   return (
     <div>
       <form>
         <label htmlFor="inpu">Inclui o texto:</label>
-        <input id="inpu" type="text" value={searchText} onChange={onSearchTextChange} />
+        <input
+          id="inpu"
+          type="text"
+          value={searchText}
+          onChange={onSearchTextChange}
+        />
         <label htmlFor="somenteFavoritos">Mostrar somente favoritos</label>
-        <input id="somenteFavoritos" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+        <input
+          id="somenteFavoritos"
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        />
         <label htmlFor="selectGenero">Filtrar por gênero</label>
-        <select id="selectGenero" value={selectedGenre} onChange={onSelectedGenreChange}>
+        <select
+          id="selectGenero"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+        >
           <option value="">Todos</option>
           <option value="action">Ação</option>
-             <option value="comedy">Comédia</option>
-          <option value="thriller">Suspense</option>          
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
         </select>
-      </form>      
+      </form>
     </div>
-  )
-}
+  );
+};
