6e55eb134ad2deca9af411523e4408bfbbbdb5c0
Fas-fCode <fasnaweb2020@gmail.com>
2020-08-14 19:19:11 -0300

    SearchBar  ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 
-export default class SearchBar extends React.Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <label>Inclui o texto:
-            <br />
-           <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}/>
-          </label>
-        </form>
-      </div>
-    )
-  }
+export default props => {
+  const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+  } = props
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="inpu">Inclui o texto:</label>
+        <input id="inpu" type="text" value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor="somenteFavoritos">Mostrar somente favoritos</label>
+        <input id="somenteFavoritos" type="checkbox" checked={ bookmarkedOnly} onChange={onBookmarkedChange}/>
+        <label htmlFor="selectGenero">Filtrar por gênero</label>
+        <select id="selectGenero" value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>          
+        </select>
+        
+      </form>
+      
+    </div>
+  )
 }
+
