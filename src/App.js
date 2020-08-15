8683fde0abe8fb2640d332090945103c715c5620
Fas-fCode <fasnaweb2020@gmail.com>
2020-08-14 22:06:43 -0300

    SearchBar  ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import Header from './components/Header';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      texto: '',
-    };
-  }
-  onSearchTextChange() {
-    return 'teste';
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <SearchBar searchText="Digite aqui" func={this.onSearchTextChange} />
-      </div>
-    );
-  }
+export default (props) => {
+  const [texto, setTexto] = useState('');
+  return (
+    <div className="App">
+      <Header />
+      <SearchBar searchText="Digite aqui" func={onSearchTextChange} />
+    </div>
+  );
+};
+
+function onSearchTextChange() {
+  return 'teste';
 }
-export default App;
