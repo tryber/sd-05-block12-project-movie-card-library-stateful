1c19cd69b658025868fe72ba9db72f0403672f2d
Cass007 <thecastieel07@gmail.com>
2020-08-16 22:26:58 -0300

    implemented SearchBar component here

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,10 +5,9 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        <h2>SearchBar</h2>
+        <h2>Addmovie</h2>
       </div>
     );
   }
 }
-
 export default SearchBar;
