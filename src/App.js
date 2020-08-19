7e050703da4ea0765eb62689e69aa41f77f8a33b
fomes <contato.felipeluna@gmail.com>
2020-08-19 16:05:12 -0300

    sbar req

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={data} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,20 @@
 // implement SearchBar component here
 import React from 'react';
+import '../SearchBar.css';
 
-export default class SearchBar {
+export default class SearchBar extends React.Component{
   render() {
     return (
-      <div>
-        <input type="text" />
+      <div className="sbar">
+        <form>
+          <br />
+          <input type="text" />
+          <br />
+          <input type="checkbox" />
+          <label for="Filmes">Filmes</label>
+          <input type="checkbox" />
+          <label for="Séries">Séries</label>
+        </form>
       </div>
     );
   }
