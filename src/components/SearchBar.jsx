c220fecf189db689633bc64448f0b0f9e4ad1eb2
fomes <contato.felipeluna@gmail.com>
2020-08-19 21:55:35 -0300

    stext dev02

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,13 +4,13 @@ import '../SearchBar.css';
 
 export default class SearchBar extends React.Component {
   render() {
-    const { searchText } = this.props;
+    const { searchText, onSearchTextChange } = this.props;
 
     return (
       <div>
         <form>
-          <label htmlFor="stext" />
-          <input type="text" value={searchText} name="stext" />
+          <label htmlFor="stext">Pesquisa</label>
+          <input type="text" value={searchText} id="stext" onChange={onSearchTextChange}/>
         </form>
       </div>
     );
