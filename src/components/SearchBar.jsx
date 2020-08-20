3fa372447b71cf4a67df58853874ad1d000a2557
fomes <contato.felipeluna@gmail.com>
2020-08-19 22:45:31 -0300

    ccfix2245

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,13 +4,13 @@ import '../SearchBar.css';
 
 export default class SearchBar extends React.Component {
   render() {
-    const { 
+    const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange 
+      onSelectedGenreChange,
     } = this.props;
 
     return (
@@ -19,16 +19,16 @@ export default class SearchBar extends React.Component {
           <label htmlFor="stext">Inclui o texto:</label>
           <input type="text" value={searchText} id="stext" onChange={onSearchTextChange} />
 
-            <label htmmlFor="cbox">Mostrar somente favoritos</label>
-            <input type="checkbox" id="cbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          <label htmmlFor="cbox">Mostrar somente favoritos</label>
+          <input type="checkbox" id="cbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
 
-            <label htmlFor="sel" value={selectedGenre} onChange={onSelectedGenreChange}>Filtrar por gênero</label>
-            <select name="select" id="sel">
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
-            </select>
+          <label htmlFor="sel" value={selectedGenre} onChange={onSelectedGenreChange}>Filtrar por gênero</label>
+          <select name="select" id="sel">
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
+          </select>
         </form>
       </div>
     );
