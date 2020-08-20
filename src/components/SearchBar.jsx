f312258719ccc5a1fa58648aa2f3d9d00a885b00
fomes <contato.felipeluna@gmail.com>
2020-08-20 16:24:14 -0300

    ccfix1624

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,14 +18,11 @@ export default class SearchBar extends React.Component {
         <form className="sbar">
           <label htmlFor="stext">Inclui o texto:</label>
           <input type="text" value={searchText} id="stext" onChange={onSearchTextChange} />
-
           <label htmlFor="cbox">Mostrar somente favoritos</label>
           <input type="checkbox" id="cbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-
           <label htmlFor="sel" value={selectedGenre} onChange={onSelectedGenreChange}>
             Filtrar por gênero
           </label>
-
           <select name="select" id="sel">
             <option value="">Todos</option>
             <option value="action">Ação</option>
