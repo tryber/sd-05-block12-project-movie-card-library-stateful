44f5c0e3cf9c86b962ea858accdd94c790d546f7
fomes <contato.felipeluna@gmail.com>
2020-08-19 22:24:01 -0300

    sbar2223

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,13 +4,31 @@ import '../SearchBar.css';
 
 export default class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, 
+      selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
       <div>
-        <form>
-          <label htmlFor="stext">Pesquisa</label>
+        <form className="sbar">
+          <div>
+          <label htmlFor="stext">Inclui o texto:</label>
           <input type="text" value={searchText} id="stext" onChange={onSearchTextChange} />
+          </div>
+
+          <div>
+            <label htmmlFor="cbox">Mostrar somente favoritos</label>
+            <input type="checkbox" id="cbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          </div>
+
+          <div>
+            <label htmlFor="sel" value={selectedGenre} onChange={onSelectedGenreChange}>Filtrar por gênero</label>
+            <select name="select" id="sel">
+              <option value="">Todos</option>
+              <option value="action">Ação</option>
+              <option value="comedy">Comédia</option>
+              <option value="thriller">Suspense</option>
+            </select>
+          </div>
         </form>
       </div>
     );
