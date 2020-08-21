4f60cb2046a72fc133a39e936b09bb58d2c699de
fomes <contato.felipeluna@gmail.com>
2020-08-21 17:03:28 -0300

    labelfix 02

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 export default class Select extends React.Component {
   render() {
     return (
-      <label htmlFor="sel">
+      <label htmlFor="sel">Escolha</label>
         <select id="sel">
           <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
         </select>
-      </label>
     );
   }
 }
