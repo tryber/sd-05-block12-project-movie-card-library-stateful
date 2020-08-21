be6967f8af8274c9765168d507cf65e1216c8367
fomes <contato.felipeluna@gmail.com>
2020-08-21 19:00:39 -0300

    movielib dev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
-import SearchBar from './components/SearchBar';
-import data from './data';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <SearchBar />
-      <MovieList movies={data} />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,27 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import data from '../data';
+
+export default class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookMarkedOnly: false,
+      selectedGenre: '',
+      movies: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={data} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
