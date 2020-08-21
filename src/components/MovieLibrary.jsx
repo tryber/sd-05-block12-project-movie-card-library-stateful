075c0c8df77973b55fdab634bb42bda6c168edd4
Fas-fCode <fasnaweb2020@gmail.com>
2020-08-21 19:10:51 -0300

    Requisito  ok

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -30,8 +30,25 @@ export default class MovieLibrary extends React.Component {
     this.setState({ selectedGenre: event.target.value });
   }
 
+  filtrarFilmes() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    if (searchText !== '') {
+      return movies.filter(movie => movie.title.indexOf(searchText) >= 0 ||
+        movie.subtitle.indexOf(searchText) >= 0 ||
+        movie.storyline.indexOf(searchText) >= 0);
+    }
+    if (bookmarkedOnly === true) {
+      return movies.filter(movie => movie.bookmarked === true);
+    }
+    if (selectedGenre !== '') {
+      return movies.filter(movie => movie.genre === selectedGenre);
+    }
+    return movies;
+  }
+
+
   render() {
-    const { movies } = this.props;
+    //const { movies } = this.props;
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return (
       <div>
@@ -43,9 +60,9 @@ export default class MovieLibrary extends React.Component {
           selectedGenre={selectedGenre}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={movies} />
+        <MovieList movies={this.filtrarFilmes()} />
         <AddMovie />
-      </div>
+      </div >
     );
   }
 }
