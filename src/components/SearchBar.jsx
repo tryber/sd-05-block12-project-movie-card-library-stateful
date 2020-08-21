f784a4db97410876b0e57d9c01a76f5567c2934d
Cass007 <thecastieel07@gmail.com>
2020-08-21 02:40:15 -0300

    madrugadao da trybe bolado cc

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,12 +19,11 @@ export default class SearchBar extends Component {
           <label htmlFor="idform">Inclui o texto:</label>
           <input id="idform" type="text" value={searchText} onChange={onSearchTextChange} />
           <label htmlFor="idform1">Mostrar somente favoritos</label>
-          <input id="idfrom1" type="checkbox"
-            checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+          <input id="idfrom1" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
           <label htmlFor="idfrom2">Filtrar por gênero</label>
-          <select
-            value={selectedGenre} onChange={onSelectedGenreChange}>{options.map((op) =>
-              (<option value={op[1]}>{op[0]}</option>))}
+          <select value={selectedGenre}
+            onChange={onSelectedGenreChange} >
+            {options.map((op) => (<option value={op[1]}>{op[0]}</option>))}
           </select>
         </form>
       </div>
