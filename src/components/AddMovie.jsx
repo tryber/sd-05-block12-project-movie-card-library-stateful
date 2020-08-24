2bca05742b3331b708d6001847afe674f501860b
fomes <contato.felipeluna@gmail.com>
2020-08-23 23:53:42 -0300

    ccfix2353

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -27,13 +27,16 @@ export default class AddMovie extends React.Component {
         <label htmlFor="tit">Título</label>
         <input type="text" id="tit" name="title" value={title} onChange={this.handleClick} />
         <label htmlFor="subtit">Subtítulo</label>
-        <input type="text" id="subtit" name="subtitle" value={subtitle} onChange={this.handleClick} />
+        <input type="text" id="subtit" name="subtitle" value={subtitle}
+          onChange={this.handleClick} />
         <label htmlFor="image">Imagem</label>
-        <input type="text" id="image" name="imagepath" value={imagePath} onChange={this.handleClick} />
+        <input type="text" id="image" name="imagepath" value={imagePath}
+          onChange={this.handleClick} />
         <label htmlFor="sinopse">Sinopse</label>
         <textarea id="sinopse" name="sinop" value={storyline} onChange={this.handleClick} />
         <label htmlFor="avaliacao">Avaliação</label>
-        <input type="number" id="avaliacao" name="aval" value={rating} onChange={this.handleClick} />
+        <input type="number" id="avaliacao" name="aval" value={rating}
+          onChange={this.handleClick} />
         <label htmlFor="genre">Gênero</label>
         <select id="genre" onChange={console.log('change')}>
           <option value="action">Ação</option>
