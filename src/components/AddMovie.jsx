60e346b11248d39a932e278d137b81de78716e2a
fomes <contato.felipeluna@gmail.com>
2020-08-23 23:50:08 -0300

    addmovie dev 23350

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,15 +11,36 @@ export default class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
     // const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating } = this.state;
 
     return (
       <form>
-        <label htmlFor="tit" />
-        <input type="text" id="tit" value={this.state.title} onChange={this.setState()} />
+        <label htmlFor="tit">Título</label>
+        <input type="text" id="tit" name="title" value={title} onChange={this.handleClick} />
+        <label htmlFor="subtit">Subtítulo</label>
+        <input type="text" id="subtit" name="subtitle" value={subtitle} onChange={this.handleClick} />
+        <label htmlFor="image">Imagem</label>
+        <input type="text" id="image" name="imagepath" value={imagePath} onChange={this.handleClick} />
+        <label htmlFor="sinopse">Sinopse</label>
+        <textarea id="sinopse" name="sinop" value={storyline} onChange={this.handleClick} />
+        <label htmlFor="avaliacao">Avaliação</label>
+        <input type="number" id="avaliacao" name="aval" value={rating} onChange={this.handleClick} />
+        <label htmlFor="genre">Gênero</label>
+        <select id="genre" onChange={console.log('change')}>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+        <button onClick={this.buttonClick}>Adicionar filme</button>
       </form>
     );
   }
