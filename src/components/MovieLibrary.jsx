db643395c4a018c0ff86d0b36b8759c1b05b158f
fomes <contato.felipeluna@gmail.com>
2020-09-21 18:36:31 -0300

    teste 1836

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,55 +7,21 @@ import data from '../data';
 export default class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
-    const { movies } = this.props;
+    // const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
       movies: [...data],
     };
-    this.search = this.search.bind(this);
-    this.mark = this.mark.bind(this);
-    this.genre = this.genre.bind(this);
+
     this.filter = this.filter.bind(this);
     this.addMovie = this.addMovie.bind(this);
   }
 
-  mark() {
-    const { bookmarkedOnly } = this.state;
-    if (bookmarkedOnly === true) {
-      return this.setState({ bookmarkedOnly: false });
-    }
-    return this.setState({ bookmarkedOnly: true });
-  }
-
-  async genre(e) {
-    const { value } = e.target;
-    await this.setState({ selectedGenre: value });
-  }
-
-  search(e) {
-    this.setState({ searchText: e.target.value });
-  }
-
   filter() {
-    const { selectedGenre, bookmarkedOnly, searchText } = this.state;
     const { movies } = this.state;
-    const select = movies;
-    if (selectedGenre !== '') {
-      return select.filter((item) => item.genre === selectedGenre);
-    }
-    if (bookmarkedOnly === true) {
-      return select.filter((item) => item.bookmarked);
-    }
-    if (searchText !== '') {
-      return select.filter((item) => (
-        (item.title.includes(searchText))
-        || (item.subtitle.includes(searchText))
-        || (item.storyline.includes(searchText))
-      ));
-    }
-    return select;
+    return movies;
   }
 
   addMovie(movie) {
